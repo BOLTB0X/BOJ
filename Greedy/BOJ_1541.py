2b418bb223c32a2b2b9662a9cbb7cc856964afe2
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-05-13 14:02:41 +0900

    백준문제_그리디알고리즘

---
+++ Greedy/BOJ_1541.py
@@ -0,0 +1,8 @@
+input_data=input().split('-') #일단 -로 나눔
+ans=0
+for i in input_data[0].split('+'):
+    ans+=int(i)
+for i in input_data[1:]:
+    for j in i.split('+'):
+        ans -= int(j)
+print(ans)
---
+++ Greedy/BOJ_2810.py
@@ -0,0 +1,8 @@
+n=int(input())
+data_input=input()
+s_cnt=data_input.count('S')
+ll_cnt=data_input.count('LL') #LL을 자석 하나로 취급
+if ll_cnt>0:
+    print(s_cnt+ll_cnt+1)
+else:
+    print(n)

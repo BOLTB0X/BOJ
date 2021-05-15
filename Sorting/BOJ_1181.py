d0c98e74c22ee34763483e6a1d50d45488bd438e
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-05-15 16:22:24 +0900

    정렬_단어 정렬
    문제
    알파벳 소문자로 이루어진 N개의 단어가 들어오면 아래와 같은 조건에 따라 정렬하는 프로그램을 작성하시오.
    
    1.길이가 짧은 것부터
    2.길이가 같으면 사전 순으로
    
    입력
    첫째 줄에 단어의 개수 N이 주어진다. (1 ≤ N ≤ 20,000) 둘째 줄부터 N개의 줄에 걸쳐 알파벳 소문자로 이루어진 단어가 한 줄에 하나씩 주어진다. 주어지는 문자열의 길이는 50을 넘지 않는다.
    
    출력
    조건에 따라 정렬하여 단어들을 출력한다. 단, 같은 단어가 여러 번 입력된 경우에는 한 번씩만 출력한다.
    
    예제 입력 1 
    13
    but
    i
    wont
    hesitate
    no
    more
    no
    more
    it
    cannot
    wait
    im
    yours
    
    예제 출력 1 
    i
    im
    it
    no
    but
    more
    wait
    wont
    yours
    cannot
    hesitate

--- Sorting/BOJ_1181.py
+++ Sorting/BOJ_1181.py
@@ -1,8 +1,9 @@
+#단어 정렬
 n=int(input())
 words=[]
 for i in range(n):
     words.append(input())
 words=list(set(words))
-ans=sorted(words,key=lambda x: (len(x),x))
+ans=sorted(words,key=lambda x: (len(x),x)) #람다식으로 정렬기준 맞춤
 for i in ans:
     print(i)

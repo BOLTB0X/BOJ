0a6ae7aa8e4585250e0a50772711b195439cae52
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-06-09 16:11:42 +0900

    백준_10820_문자열 분석
    https://www.acmicpc.net/problem/10820

---
+++ C++배우기(151~200)/BOJ_10820.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void)
+{
+    for (int i = 0; i < 100; i++)
+    {
+        string s;
+        getline(cin, s);
+
+        if (s.length() == 0) break;
+        int upper = 0, lower = 0, number = 0, space = 0;
+        for (int j = 0; j < s.length(); j++)
+
+            if (s[j] >= 'A' && s[j] <= 'Z') upper+=1;
+
+            else if (s[j] >= 'a' && s[j] <= 'z') lower+=1;
+
+            else if (s[j] >= '0' && s[j] <= '9') number+=1;
+
+            else if (s[j] == ' ') space+=1;
+        cout << lower << " " << upper << " " << number << " " << space << "\n";
+    }
+    return 0;
+}

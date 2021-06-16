7a21af9f2f1c27d96045c0a5c48ea4e3d6bf597e
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-06-16 11:43:48 +0900

    바이러스
    https://www.acmicpc.net/problem/2606

--- Graph_Traversal/BOJ_2606(바이러스).py
+++ Graph_Traversal/BOJ_2606(바이러스).py
@@ -1,3 +1,4 @@
+#BFS
 from collections import deque
 n=int(input())
 m=int(input())
@@ -26,3 +27,24 @@ def bfs(graph,visited,answer):
                 answer+=1
     return answer
 print(bfs(graph,visited,answer))
+
+#DFS
+n=int(input())
+m=int(input())
+graph=[[]*i for i in range(n+1)]
+visited=[False]*(n+1)
+answer=[]
+for _ in range(m): #그래프 생성
+    a,b=map(int,input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+    graph[a].sort()
+    graph[b].sort()
+def dfs(graph,start,visited,answer):
+    visited[start]=True
+    for i in graph[start]:
+        if visited[i]==False:
+            answer.append(i)
+            dfs(graph,i,visited,answer)
+    return len(answer)
+print(dfs(graph,1,visited,answer))

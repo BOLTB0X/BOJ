91d86fe6332009d94fec759bdc3916d804b2bece
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-07-03 14:13:27 +0900

    등장하지 않은 문자의 합
    https://www.acmicpc.net/problem/3059

--- C++배우기(151~200)/3059(등장하지 않는 문자의 합).cpp
+++ C++배우기(151~200)/3059(등장하지 않는 문자의 합).cpp
@@ -1,24 +1,28 @@
-#include <iostream> 
-#include <string> 
-using namespace std; 
+#include <iostream>
+#include <string>
+using namespace std;
 
-int main(void) { 
-	int t, tot, len; 
-	cin >> t; 
-	for (int i = 0; i < t; i++) {
-		string s; 
-		bool check[26] = { false, }; 
+int T;//테스트 변수
 
-		cin >> s; 
-		tot = 0; 
-		len = s.length();
-		for (int j = 0; j < len; j++) 
-			check[(char)s[j] - 'A'] = true; 
-		for (int j = 0; j < 26; j++) 
-			if (!check[j]) 
-				tot += (j + 65); 
+void solution(void) {
+	cin >> T;
+	for (int i = 0; i < T; i++) {
+		string s; //문자열
+		bool check[26] = { false, }; //체크 배열
 
-		cout << tot << '\n'; 
+		cin >> s;
+		int tot = 0;
+		for (int j = 0; j < s.length(); j++) 
+			check[(char)s[j] - 'A'] = true; //입력받은 문자열 알파벳을 체크
+		for (int j = 0; j < 26; j++) { //나오지 않는 것들 합해야 하므로
+			if (!check[j])
+				tot += (j + 65); //A의 아스키 코드 65
+		}
+		cout << tot << '\n';
 	}
+}
+
+int main(void) {
+	solution();
 	return 0;
 }

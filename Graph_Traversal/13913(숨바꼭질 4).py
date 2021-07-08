ffdade4e9f297155f4bded5b13810f5131905d9a
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-07-08 15:47:13 +0900

    숨바꼭질 4
    https://www.acmicpc.net/problem/13913

--- Graph_Traversal/13913(숨바꼭질 4).py
+++ Graph_Traversal/13913(숨바꼭질 4).py
@@ -1,29 +1,32 @@
 from collections import deque
 MAX=100001
+
 def path(x):
-    arr = []
-    temp = x
-    for _ in range(dist[x]+1):
-        arr.append(temp)
-        temp = move[temp]
-    print(' '.join(map(str, arr[::-1])))
+    p= [] #다녀온 노드를 담는 리스트
+    tmp=x
+    for _ in range(check[x]+1):
+        p.append(tmp)
+        tmp=move[tmp] #x의 이전 노드로 저장 그 다음 이전노드의 이전노드가 저장 ....
+    
+    print(' '.join(map(str, p[::-1]))) #역순으로 저장되니
 
 def bfs(n,k):
-    q = deque()
-    q.append(n)
-    while q:
-        x = q.popleft()
-        if x == k:
-            print(dist[x])
+    que = deque()
+    que.append(n)
+
+    while que:
+        x=que.popleft()
+        if x==k:
+            print(check[x])
             path(x)
-            return x
-        for i in (x+1, x-1, 2*x):
-            if 0<=i<=100000 and dist[i]==0:
-                q.append(i)
-                dist[i] = dist[x] + 1
-                move[i] = x
+            break
+        for nx in [x-1,x+1,x*2]:
+            if 0<=nx<MAX and check[nx]==0:
+                que.append(nx)
+                check[nx]=check[x]+1 #걸린 시간 저장
+                move[nx]=x #이전 노드 저장
 
 n, k = map(int, input().split())
-dist = [0]*MAX
-move = [0]*MAX
+check = [0]*MAX #걸리는 시간을 담는 리스트
+move = [0]*MAX #이전 거쳐온 노드를 담는 리스트
 bfs(n,k)

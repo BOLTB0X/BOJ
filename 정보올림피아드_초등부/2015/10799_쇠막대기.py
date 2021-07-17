f996d91d65bc494993738b26b3acac4ba13fea6c
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-07-17 15:29:25 +0900

    Add files via upload

---
+++ 정보올림피아드_초등부/2015/10799_쇠막대기.py
@@ -0,0 +1,18 @@
+bar_razor = list(input())
+answer = 0 
+stack = []
+
+for i in range(len(bar_razor)):
+    if bar_razor[i]=='(':
+        stack.append('(') #스택에 넣는다.
+    
+    else: #()이 되면 절단 시작
+        if bar_razor[i-1] == '(': # 바로 앞이 '('
+            stack.pop() #바로앞 '('은 카운트 제외
+            answer += len(stack)
+        
+        else: # ')'인경우
+            stack.pop()
+            answer+=1 #하나만 잘림
+
+print(answer)
---
+++ 정보올림피아드_초등부/2015/10800_컬러볼.py
@@ -0,0 +1,6 @@
+ball_inf=[]
+T=int(input())
+for i in range(1,T+1):
+    Ci,Si=map(int,input().split())
+    ball_inf.append([i,Ci,Si]) #번호, 색, 크기
+     

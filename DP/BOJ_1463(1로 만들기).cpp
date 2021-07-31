f62cd7aa53ba009e60012c93a7c674486be7fa13
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-07-31 16:26:56 +0900

    1로 만들기

--- DP/BOJ_1463(1로 만들기).cpp
+++ DP/BOJ_1463(1로 만들기).cpp
@@ -1,19 +1,27 @@
 #include <iostream>
 using namespace std;
 
-int dp[1000000] = { 0, };
+int dp[1000001] = { 0, };
 int n;
 
-int main(void) {
+int min(int value1, int value2) {
+	return value1 < value2 ? value1 : value2;
+}
+int solution(void) {
 	cin >> n;
-	for (int i=2;i<=n;i++) {
+
+	for (int i = 2; i <= n; i++) {
 		dp[i] = dp[i - 1] + 1;
-		if (i % 3 == 0 && dp[i] > dp[i / 3] + 1) {
-			dp[i] = dp[i / 3]+1;
-		}
-		if (i % 2 == 0 && dp[i] > dp[i / 2] + 1) {
-			dp[i] = dp[i / 2]+1;
-		}
+		if (i % 3 == 0)
+			dp[i] = min(dp[i], dp[i / 3] + 1);
+		if (i % 2 == 0)
+			dp[i] = min(dp[i], dp[i / 2] + 1);
 	}
-	cout << dp[n] << '\n';
+
+	return dp[n];
+}
+
+int main(void) {
+	cout << solution() << '\n';
+	return 0;
 }

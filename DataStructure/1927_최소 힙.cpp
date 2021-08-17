37b90a7dbb31f1a351a8c15d53fa3004b8d5b164
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-08-17 21:12:57 +0900

    최소 힙
    https://www.acmicpc.net/problem/1927

---
+++ DataStructure/1927_최소 힙.cpp
@@ -0,0 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int n;
+struct compare {
+	bool operator() (int x, int y) {
+		return x > y;
+	}
+};
+
+priority_queue<int,vector<int>,compare> pq;
+
+int main(void) {
+	int value;
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &value);
+
+		if (value == 0) {
+			if (pq.empty())
+				printf("0\n");
+			else {
+				int p = pq.top();
+				pq.pop();
+				printf("%d\n", p);
+			}
+		}
+		else {
+			pq.push(value);
+		}
+	}
+	return 0;
+}

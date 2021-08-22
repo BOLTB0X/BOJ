dda7121da696be0bec23af73ed4571a5605ce2ce
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-08-22 16:31:06 +0900

    정수 삼각형
    https://www.acmicpc.net/problem/1932

---
+++ DP/1932_정수 삼각형.cpp
@@ -0,0 +1,44 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <cstdio>
+using namespace std;
+
+int n;
+int graph[501][501];
+int dp[501][501] = { 0, };
+
+int max(int a, int b) {
+	return a > b ? a : b;
+}
+
+int solution(int n) {
+	//트리의 최상위 셋팅
+	dp[1][1] = graph[1][1];
+	//순회 시작
+	for (int r = 2; r <= n; r++) {
+		for (int c = 1; c <= r; c++) 
+			dp[r][c] = max(dp[r - 1][c - 1], dp[r - 1][c]) + graph[r][c];
+	}
+	//최하위 구간 최댓값 찾기
+	int max_value = dp[n][1];
+	for (int i = 2; i <= n; i++) {
+		if (max_value < dp[n][i])
+			max_value = dp[n][i];
+	}
+
+	return max_value;
+}
+
+int main(void) {
+	scanf("%d", &n);
+	
+	//그래프 생성
+	for (int r = 1; r <= n; r++) {
+		for (int c = 1; c <= r; c++) {
+			scanf("%d", &graph[r][c]);
+		}
+	}
+	int ret=solution(n);
+
+	printf("%d", ret);
+	return 0;
+}

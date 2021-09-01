4d52f5dba0c0b3a12799c4d63966cbdf639af3cf
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-09-01 23:37:00 +0900

    진법 변환 2
    https://www.acmicpc.net/problem/11005

---
+++ Implementation/11005_진법 변환2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void base(int n, int b) {
+    if (n != 0) {
+        base(n / b, b);
+        if (n % b > 9 && b > 10) // modified
+            printf("%c", 'A' + (n % b) - 10);
+        else
+            printf("%d", n % b);
+    }
+}
+int main() {
+    int N, B;
+    scanf("%d %d", &N, &B);
+    base(N, B);
+    printf("\n");
+}

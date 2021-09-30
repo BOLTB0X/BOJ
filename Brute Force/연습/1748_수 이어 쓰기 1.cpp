66160784bf846b8c43c85edd1a7d0720aa05e5a1
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-09-30 22:17:55 +0900

    수 이어 쓰기 1
    https://www.acmicpc.net/problem/1748

---
+++ Brute Force/연습/1748_수 이어 쓰기 1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+	int n;
+	int i = 1, answer = 0;
+
+	cin >> n;
+	while (i <= n) {
+		answer += (n - i + 1);
+		i *= 10;
+	}
+	cout << answer << '\n';
+	return 0;
+}

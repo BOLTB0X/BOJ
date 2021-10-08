4a21050ec210b4fe94d06edf71b68fcc298ebbd5
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-10-08 22:15:45 +0900

    벽 부수고 이동하기
    https://www.acmicpc.net/problem/2206

--- Graph_Traversal/2206_벽 부수고 이동하기.cpp
+++ Graph_Traversal/2206_벽 부수고 이동하기.cpp
@@ -1,50 +1,64 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <cstdio>
 #include <queue>
 using namespace std;
 
-typedef struct {
-	int r, c;
+struct NODE{
+	int y, x;
 	int crush_cnt;
 	int move_cnt;
-} inf;
+};
 
-int N, M;
-int map[1001][1001];
-bool visited[1001][1001][2]; //�湮 �� ���� �μ� ���
-queue<inf> q;
-const int dr[4] = { -1,0,1,0 };
-const int dc[4] = { 0,1,0,-1 };
+int n, m;
+int board[1001][1001];
+//방문 및 벽을 부순 경우
+bool visited[1001][1001][2]; 
 
+//상하좌우
+const int dy[4] = { 1,-1,0,0 };
+const int dx[4] = { 0,0,-1,1 };
+
+//입력
+void input(void) {
+	scanf("%d %d", &n, &m);
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= m; j++) {
+			scanf("%1d", &board[i][j]);
+		}
+	}
+}
+
+//범위체크
 bool is_range(int row, int col) {
-	if (row <1 || col < 1 || row >N || col > M)
+	if (row <1 || col < 1 || row >n || col > m)
 		return false;
 	else
 		return true;
- }
+}
 
-int BFS() {
+//너비우선
+int BFS(queue<NODE> &q) {
 	q.push({ 1,1,0,1 });
 	visited[1][1][0] = true;
+	
 	while (!q.empty()) {
-		inf cur = q.front();
+		NODE cur = q.front();
 		q.pop();
-
-		if (cur.r == N && cur.c == M) {
+		//탈출 조건
+		if (cur.y == n && cur.x == m) {
 			return cur.move_cnt;
 		}
-
+		//상하좌우 탐색
 		for (int dir = 0; dir < 4; dir++) {
-			int nr = cur.r + dr[dir];
-			int nc = cur.c + dc[dir];
-			if (is_range(nr, nc)) {
-				if (map[nr][nc] == 1 && cur.crush_cnt == 0) {
-					visited[nr][nc][cur.crush_cnt] = true;
-					q.push({ nr,nc,cur.crush_cnt + 1,cur.move_cnt + 1 });
+			int ny = cur.y + dy[dir];
+			int nx = cur.x + dx[dir];
+			if (is_range(ny, nx)) {
+				if (board[ny][nx] == 1 && cur.crush_cnt == 0) {
+					visited[ny][nx][cur.crush_cnt] = true;
+					q.push({ ny,nx,cur.crush_cnt + 1,cur.move_cnt + 1 });
 				}
-				if (map[nr][nc] == 0 && !visited[nr][nc][cur.crush_cnt]) {
-					visited[nr][nc][cur.crush_cnt] = true;
-					q.push({ nr,nc,cur.crush_cnt,cur.move_cnt + 1 });
+				if (board[ny][nx] == 0 && !visited[ny][nx][cur.crush_cnt]) {
+					visited[ny][nx][cur.crush_cnt] = true;
+					q.push({ ny,nx,cur.crush_cnt,cur.move_cnt + 1 });
 				}
 			}
 		}
@@ -54,14 +68,12 @@ int BFS() {
 }
 
 int main(void) {
-	scanf("%d %d", &N, &M);
-	for (int i = 1; i <= N; i++) {
-		for (int j = 1; j <= M; j++) {
-			scanf("%1d", &map[i][j]);
-		}
-	}
-
-	int ret = BFS();
+	//입력
+	input();
+	queue<NODE> q;
+	//BFS로 정답 반환
+	int ret = BFS(q);
+	//정답 출력
 	printf("%d", ret);
 	return 0;
 }

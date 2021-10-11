6defc2f9433c4642cc4e1b1db78ee2dfb8cbfe35
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-10-11 21:43:23 +0900

    수들의 합
    https://www.acmicpc.net/problem/1789

--- Greedy/1789_수들의 합.c
+++ Greedy/1789_수들의 합.c
@@ -2,15 +2,19 @@
 
 int main(void) {
 	long long S;
+	//입력
 	scanf("%lld", &S);
 
 	long long tot = 0;
 	int answer = 0;
+	//1부터 시작
 	int number = 1;
-
+	
 	while(1) {
 		tot += number;
 		answer++;
+		//탈출조건
+		//S보다 커지는지 검사
 		if (tot > S) {
 			answer--;
 			break;

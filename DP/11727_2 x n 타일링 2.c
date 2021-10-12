cecb969b1a78364944a84733abfdb21b5563e8fd
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-10-12 14:35:39 +0900

    2×n 타일링 2
    https://www.acmicpc.net/problem/11727

---
+++ DP/11727_2 x n 타일링 2.c
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	int n;
+	scanf("%d", &n);
+
+	//동적할당 및 초기화
+	long long* dp = (long long*)malloc(sizeof(long long) * (n + 1));
+	for (int i = 0; i <= n; ++i) {
+		dp[i] = 0;
+	}
+	dp[1] = 1;
+	dp[2] = 3;
+
+	//보텀업 방식
+	for (int i = 3; i <= n; ++i)
+		//점화식 i-2의 *2배 해주고 더해야함
+		dp[i] = (dp[i - 1] + (dp[i - 2] * 2)) % 10007;
+	
+	// 10007로 나눈 나머지를 출력
+	printf("%lld\n", dp[n]);
+	free(dp);
+	return 0;
+}

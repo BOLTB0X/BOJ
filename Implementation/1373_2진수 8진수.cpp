8701002ad742d68f160a544900d8ffdc6d8215d5
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-10-14 20:57:40 +0900

    2진수 8진수
    https://www.acmicpc.net/problem/1373

---
+++ Implementation/1373_2진수 8진수.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    string s = "";
+    // string으로 입력
+    getline(cin, s);
+    // 자릿수를 3으로 나누어 떨어지게 만들기
+    while (s.size() % 3 != 0) {
+        s = '0' + s;
+    }
+    string answer = "";
+    for (int i = 0; i < s.size(); i += 3) {
+        int tmp = 0;
+        // 3자리씩 8진수로 변환하기
+        for (int j = i; j < i + 3; j++) {
+            tmp += (s[j] - '0') * pow(2, 2 - (j % 3));
+        }
+        answer += to_string(tmp);
+    }
+    cout << answer << '\n';
+    return 0;
+}

5b4af742c2e9d9609137e2249ce2ce210a6311d1
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-10-14 22:04:39 +0900

    진법 변환
    https://www.acmicpc.net/problem/2745

--- Implementation/2745_진법 변환.cpp
+++ Implementation/2745_진법 변환.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <string>
+#include <cmath>
 
 using namespace std;
 
 int pow(int a, int b) {
 	int res = 1;
-	if (b == 0)
+	if (b == 0) 
 		return 1;
 	else {
 		for (int i = 0; i < a; i++)
@@ -14,20 +15,35 @@ int pow(int a, int b) {
 	return res;
 }
 
+//b진수 n을 10진수로
+int trans_dec(string n, int b) {
+	int tot = 0;
+	int size = n.length();
+
+	for (int i = size - 1; i >= 0; i--) {
+		//알파벳이면
+		if (n[i] >= 'A' && n[i] <= 'Z') 
+			//+10
+			tot += (n[i] - 'A' + 10) * pow(b, size - 1 - i);
+		
+		else
+			tot += (n[i] - '0') * pow(b, size - 1 - i);
+	}
+	return tot;
+}
+
 int main(void) {
-	string N = "";
+	//초기화
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	string N;
 	int B;
-	int answer = 0;
 
 	cin >> N >> B;
 
-	for (int i = N.length() - 1; i >= 0; i--) {
-		if (N[i] >= 'A' && N[i] <= 'Z')
-			answer += (N[i] - 'A' + 10) * pow(B, N.length() - 1 - i);
-		else
-			answer += (N[i] - '0') * pow(B, N.length() - 1 - i);
-	}
-	
-	cout << answer << '\n';
+	int ret = trans_dec(N, B);
+	cout << ret << '\n';
 	return 0;
 }

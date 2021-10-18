cb510f4bab61041d435575019970c61e915eb926
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-10-18 14:58:18 +0900

    계단 오르기
    https://www.acmicpc.net/problem/2579

--- DP/2579_계단 오르기.c
+++ DP/2579_계단 오르기.c
@@ -4,6 +4,7 @@
 int step[301] = { 0, };
 int dp[301] = { 0, };
 
+//최대 비교
 int max(int a, int b) {
 	return a > b ? a : b;
 }
@@ -16,11 +17,13 @@ int main(void) {
 		scanf("%d", &step[i]);
 		dp[i] = 0;
 	}
-
+	
+	//각 계단오르는 수 메모이제이션에 저장
 	dp[0] = step[0];
 	dp[1] = max(step[0], step[0] + step[1]);
 	dp[2] = max(step[1] + step[2], step[0] + step[2]);
-
+	
+	//한칸인 경우와 두칸 건너뛴 경우 중 큰값으로
 	for (int i = 3; i < n; ++i) 
 		dp[i] = max(step[i] + dp[i - 2], step[i] + step[i - 1] + dp[i - 3]);
 	

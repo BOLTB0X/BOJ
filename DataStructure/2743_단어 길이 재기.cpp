6aa8872a35a98bb033bc989b0762cd8c6d45e007
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-10-25 15:00:40 +0900

    단어 길이 재기
    https://www.acmicpc.net/problem/2743

---
+++ DataStructure/2743_단어 길이 재기.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void solution(void) {
+	string str;
+	cin >> str;
+
+	cout << str.length() << '\n';
+	return;
+}
+
+int main(void) {
+	//ÃÊ±âÈ­
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	solution();
+	return 0;
+}

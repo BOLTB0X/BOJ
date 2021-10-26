2786f3ca4c5993749671e207e3dbcdc046c55e7d
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-10-26 13:05:34 +0900

    오큰수
    https://www.acmicpc.net/problem/17298

--- DataStructure/17298_오큰수.py
+++ DataStructure/17298_오큰수.py
@@ -1,15 +1,19 @@
+#덱 이용
 from collections import deque
 
 n=int(input())
 seq =list(map(int,input().split()))
 
+#초기화
 nge = [-1 for _ in range(n)]
 stack=deque()
 
 for i in range(n):
+    #스택이 top이 다 더 커질 때까지 스택을 pop
     while stack and (stack[-1][0] < seq[i]):
         tmp,idx =stack.pop()
         nge[idx]=seq[i]
     stack.append([seq[i],i])
 
+#출력
 print(*nge)

010f4e9e97851162e060eb684c7a2d454ea28492
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-10-27 15:50:06 +0900

    연산자 끼워넣기
    https://www.acmicpc.net/problem/14888

---
+++ Brute Force/기초/14888_연산자 끼워넣기.cpp
@@ -0,0 +1,51 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <cstdio>
+
+using namespace std;
+
+//최대
+int max(int a, int b) {
+	return a > b ? a : b;
+}
+//최소
+int min(int a, int b) {
+	return a < b ? a : b;
+}
+
+int n, max_result = -0x7fffffff, min_result=0x7fffffff;
+int numbers[11];
+
+void DFS(int tot, int add, int sub, int mul, int div,int idx) {
+	if (idx == n) {
+		max_result = max(max_result, tot);
+		min_result = min(min_result, tot);
+		return;
+	}
+	else {
+		if (add > 0) {
+			DFS(tot + numbers[idx], add - 1, sub, mul, div, idx + 1);
+		}
+		if (sub > 0) {
+			DFS(tot - numbers[idx], add, sub - 1, mul, div, idx + 1);
+		}
+		if (mul > 0) {
+			DFS(tot * numbers[idx], add, sub, mul - 1, div, idx + 1);
+		}
+		if (div > 0) {
+			DFS((int)tot / numbers[idx], add, sub, mul, div - 1, idx + 1);
+		}
+	}
+}
+
+int main(void) {
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &numbers[i]);
+	}
+	int add, sub, mul, div;
+	scanf("%d %d %d %d", &add, &sub, &mul, &div);
+	DFS(numbers[0], add, sub, mul, div, 1);
+	printf("%d\n", max_result);
+	printf("%d\n", min_result);
+	return 0;
+}
---
+++ Brute Force/기초/14888_연산자 끼워넣기.py
@@ -0,0 +1,32 @@
+#입출력
+from sys import stdin
+#입력 
+input = stdin.readline
+
+n=int(input())
+nums=list(map(int,input().split()))
+add,sub,mul,div=map(int,input().split())
+# + - * // 
+
+min_v=1000000001
+max_v=-1000000001
+
+def cal(num,idx,add,sub,mul,div):
+    global n, max_v, min_v
+    if idx == n:
+        max_v=max(num, max_v)
+        min_v=min(num, min_v)
+        return
+    else:
+        if add > 0:
+            cal(num + nums[idx], idx + 1, add - 1, sub, mul, div)
+        if sub > 0:
+            cal(num - nums[idx], idx + 1, add, sub - 1, mul, div)
+        if mul > 0:
+            cal(num * nums[idx], idx + 1, add, sub , mul -1, div)
+        if div > 0:
+            cal(int(num / nums[idx]), idx + 1, add, sub, mul, div -1)
+
+cal(nums[0],1,add,sub,mul,div)
+print(max_v)
+print(min_v)

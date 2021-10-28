9cf964c11e53e664a4461d2075eb38798cc90f5c
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-10-28 13:41:51 +0900

    최대 힙
    https://www.acmicpc.net/problem/11279

---
+++ DataStructure/11279_최대 힙.py
@@ -0,0 +1,18 @@
+import heapq
+#입출력
+from sys import stdin
+#입력 
+input = stdin.readline
+
+T = int(input())
+heap = []
+
+for _ in range(T):
+    x = int(input())
+    if x == 0:
+        if heap:
+            print(heapq.heappop(heap)[1])
+        else:
+            print('0')
+    else:
+        heapq.heappush(heap, [-x,x])

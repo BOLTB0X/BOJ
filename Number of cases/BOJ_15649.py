44fad41711f16c81afe2fbf446955e4141d56a4a
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-10-29 15:21:23 +0900

    Delete BOJ_15649.py

--- Number of cases/BOJ_15649.py
+++
@@ -1,8 +0,0 @@
-#N과 M(1)
-from itertools import permutations
-import sys
-input=lambda : sys.stdin.readline().rstrip()
-n,m=map(int,input().split())
-P=list(permutations([i for i in range(1,n+1)],m))
-for i in P:
-    print(*i)

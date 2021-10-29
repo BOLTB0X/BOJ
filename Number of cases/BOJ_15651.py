5b36a67facc2cb4347f64abf8d91098dea16c61d
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-10-29 15:29:47 +0900

    Delete BOJ_15651.py

--- Number of cases/BOJ_15651.py
+++
@@ -1,8 +0,0 @@
-#N과 M(3)
-from itertools import product
-import sys
-input = lambda :sys.stdin.readline().rstrip()
-n,m=map(int,input().split())
-P=list(product([i for i in range(1,n+1)],repeat=m))
-for i in P:
-    print(*i)

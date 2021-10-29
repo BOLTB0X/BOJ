30452ec1c4386fc6e81b0f70d100be2647a9a5f5
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-10-29 15:25:18 +0900

    Delete BOJ_15650.py

--- Number of cases/BOJ_15650.py
+++
@@ -1,8 +0,0 @@
-# N,M(2) 조합
-from itertools import combinations
-import sys
-input = lambda :sys.stdin.readline().rstrip()
-n,m=map(int,input().split())
-C=list(combinations(range(1,n+1),m))
-for i in C:
-    print(*i)

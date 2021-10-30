76edc4f932fbfb99ee7f39773dc43bdf7f420cb5
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-10-30 20:23:40 +0900

    Delete 6603_로또.py

--- Brute Force/기초/6603_로또.py
+++
@@ -1,8 +0,0 @@
-from itertools import combinations
-while True:
-    n,*com=map(int,input().split())
-    if n==0:
-        break
-    C=list(combinations(com,6))
-    for c in C:
-        print(*c)

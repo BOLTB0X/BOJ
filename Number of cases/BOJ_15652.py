7c08f615b40793cfc9cf6efaa65cf93b2d95b1f7
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-10-30 20:19:52 +0900

    Delete BOJ_15652.py

--- Number of cases/BOJ_15652.py
+++
@@ -1,8 +0,0 @@
-#N과M (4)
-from itertools import combinations_with_replacement as com
-import sys
-input=lambda :sys.stdin.readline().rstrip()
-n,m=map(int,input().split())
-C=list(com(range(1,n+1),m))
-for i in C:
-    print(*i)

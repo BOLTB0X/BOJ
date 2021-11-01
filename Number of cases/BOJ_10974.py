91a4165916b8bc47586a8bfc4b2526ec91d2958e
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-11-01 14:32:41 +0900

    Delete BOJ_10974.py

--- Number of cases/BOJ_10974.py
+++
@@ -1,8 +0,0 @@
-#모든순열
-from itertools import permutations
-n=int(input())
-p=list(permutations([i for i in range(1,n+1)], n))
-for i in range(len(p)):
-    for j in p[i]:
-        print(j,end=' ')
-    print()

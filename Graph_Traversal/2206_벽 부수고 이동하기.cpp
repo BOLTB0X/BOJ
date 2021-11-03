fb96b33e7b4bb10ba42d267eba6dd0a7f35409b3
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-11-03 09:58:29 +0900

    벽 부수고 이동하기
    https://www.acmicpc.net/problem/2206

--- Graph_Traversal/2206_벽 부수고 이동하기.cpp
+++ Graph_Traversal/2206_벽 부수고 이동하기.cpp
@@ -1,7 +1,11 @@
-#include <cstdio>
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
 #include <queue>
+
 using namespace std;
 
+//좌표,부순지체크, 이동횟수
 struct NODE{
 	int y, x;
 	int crush_cnt;
@@ -12,48 +16,33 @@ int n, m;
 int board[1001][1001];
 //방문 및 벽을 부순 경우
 bool visited[1001][1001][2]; 
-
-//상하좌우
-const int dy[4] = { 1,-1,0,0 };
-const int dx[4] = { 0,0,-1,1 };
-
-//입력
-void input(void) {
-	scanf("%d %d", &n, &m);
-	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= m; j++) {
-			scanf("%1d", &board[i][j]);
-		}
-	}
-}
-
-//범위체크
-bool is_range(int row, int col) {
-	if (row <1 || col < 1 || row >n || col > m)
-		return false;
-	else
-		return true;
-}
+queue<NODE> q;
 
 //너비우선
-int BFS(queue<NODE> &q) {
-	q.push({ 1,1,0,1 });
-	visited[1][1][0] = true;
-	
+int BFS(void) {
+	//상하좌우
+	const int dy[4] = { 1,-1,0,0 };
+	const int dx[4] = { 0,0,-1,1 };
+
+	q.push({ 0,0,0,1 });
+	visited[0][0][0] = true;
+
 	while (!q.empty()) {
 		NODE cur = q.front();
 		q.pop();
 		//탈출 조건
-		if (cur.y == n && cur.x == m) {
+		if (cur.y == n - 1 && cur.x == m - 1) 
 			return cur.move_cnt;
-		}
+		
 		//상하좌우 탐색
 		for (int dir = 0; dir < 4; dir++) {
 			int ny = cur.y + dy[dir];
 			int nx = cur.x + dx[dir];
-			if (is_range(ny, nx)) {
+
+			//범위 안에 들어옴
+			if (ny >= 0 && nx >= 0 && ny < n && nx < m) {
 				if (board[ny][nx] == 1 && cur.crush_cnt == 0) {
-					visited[ny][nx][cur.crush_cnt] = true;
+					visited[ny][nx][cur.crush_cnt + 1] = true;
 					q.push({ ny,nx,cur.crush_cnt + 1,cur.move_cnt + 1 });
 				}
 				if (board[ny][nx] == 0 && !visited[ny][nx][cur.crush_cnt]) {
@@ -64,16 +53,32 @@ int BFS(queue<NODE> &q) {
 		}
 	}
 	return -1;
+}
 
+void solution(void) {
+	//입력
+	string tmp;
+
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) {
+		cin >> tmp;
+		for (int j = 0; j < m; j++) 
+			board[i][j] = tmp[j] - '0';
+	}
+	
+	//BFS로 정답 반환
+	int ret = BFS();
+
+	cout << ret << '\n';
+	return;
 }
 
 int main(void) {
-	//입력
-	input();
-	queue<NODE> q;
-	//BFS로 정답 반환
-	int ret = BFS(q);
-	//정답 출력
-	printf("%d", ret);
+	//초기화
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	solution();
 	return 0;
 }

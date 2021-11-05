e9528d7e42de41ceb48c84911f4ee2bb6afdbd13
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-11-05 14:10:13 +0900

    연속 합
    https://www.acmicpc.net/problem/1912

--- DP/1912_연속 합.cpp
+++ DP/1912_연속 합.cpp
@@ -2,6 +2,9 @@
 
 using namespace std;
 
+int arr[100000];
+int dp[100000] = { 0, };
+
 //최댓값 비교
 int max(int a, int b) {
 	return a > b ? a : b;
@@ -11,28 +14,21 @@ void solution(void) {
 	int n;
 	cin >> n;
 
-	//동적할당
-	int* arr = new int[n];
-	int* dp = new int[n];
-
-	for (int i = 0; i < n; ++i) {
+	for (int i = 0; i < n; ++i)
 		cin >> arr[i];
-		dp[i] = 0;
-	}
+
+	int max_value = arr[0];
+	dp[0] = arr[0];
+
 	//보텀업
-	int max_value = dp[0] = arr[0];
 	for (int i = 1; i < n; ++i) {
-		dp[i] = max(arr[i], dp[i-1] + arr[i]);
-		//최댓값 찾기
-		if (max_value < dp[i])
+		dp[i] = max(arr[i], arr[i] + dp[i - 1]);
+
+		if (dp[i] > max_value)
 			max_value = dp[i];
 	}
-	//정답 출력
-	cout << max_value << '\n';
 
-	//메모리 해제
-	delete[] dp;
-	delete[] arr;
+	cout << max_value << '\n';
 	return;
 }
 

407a317148878d7919597b2a836ad4a62a981d43
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-11-06 15:38:28 +0900

    Delete BOJ_1181.py

--- Sorting/BOJ_1181.py
+++
@@ -1,9 +0,0 @@
-#단어 정렬
-n=int(input())
-words=[]
-for i in range(n):
-    words.append(input())
-words=list(set(words))
-ans=sorted(words,key=lambda x: (len(x),x)) #람다식으로 정렬기준 맞춤
-for i in ans:
-    print(i)

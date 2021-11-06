9b3af8b74804d64f47fd26eb8407f2aab6cae523
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-11-06 15:29:50 +0900

    나이순 정렬
    https://www.acmicpc.net/problem/10814

--- Sorting/10814_나이순 정렬.cpp
+++ Sorting/10814_나이순 정렬.cpp
@@ -8,7 +8,7 @@ bool compare(pair<int, string> a, pair<int, string> b) {
 	return a.first < b.first;
 }
 
-int main(void) {
+void solution(void) {
 	int n;
 	vector<pair<int, string>> arr;
 	
@@ -21,11 +21,22 @@ int main(void) {
 		cin >> age >> name;
 		arr.push_back({age,name});
 	}
+	//정렬
+	stable_sort(arr.begin(), arr.end(), compare);
 
-	sort(arr.begin(), arr.end(), compare);
-
-	for (int i = 0; i < n; i++) {
+	//출력
+	for (int i = 0; i < n; i++) 
 		cout << arr[i].first << ' ' << arr[i].second << '\n';
-	}
+	
+	return;
+}
+
+int main(void) {
+	//초기화
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	solution();
 	return 0;
 }

246508457e7d00b6460a091fac3f388c377dbd79
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-11-07 13:36:16 +0900

    좌표 정렬하기 2
    https://www.acmicpc.net/problem/11651

--- Sorting/11651_좌표 정렬하기 2.cpp
+++ Sorting/11651_좌표 정렬하기 2.cpp
@@ -1,22 +1,37 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+
 using namespace std;
 
-int main(void) {
+void solution(void) {
 	int n;
-	
 	cin >> n;
+
+	//벡터 초기화
+	//2차원 벡터
 	vector<vector<int>> arr(n, vector<int>(2, 0));
 	
-	for (int i = 0; i < n; i++) {
+	//입력
+	for (int i = 0; i < n; i++) 
 		cin >> arr[i][1] >> arr[i][0];
-	}
 
+	//정렬
 	sort(arr.begin(), arr.end());
 
-	for (int i = 0; i < n; i++) {
+	//출력만 바꿔서
+	for (int i = 0; i < n; i++) 
 		cout << arr[i][1] << ' ' << arr[i][0] << '\n';
-	}
+	
+	return;
+}
+
+int main(void) {
+	//초기화
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	solution();
 	return 0;
 }

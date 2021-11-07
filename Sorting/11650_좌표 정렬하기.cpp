40863c00901ab20db2c8e9bf18f8db70f82487fa
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-11-07 13:22:38 +0900

    좌표 정렬하기
    https://www.acmicpc.net/problem/11650

--- Sorting/11650_좌표 정렬하기.cpp
+++ Sorting/11650_좌표 정렬하기.cpp
@@ -1,23 +1,37 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+
 using namespace std;
 
-int n;
-
 void solution() {
+	int n;
 	cin >> n;
-	vector<vector<int>> arr(n,vector<int>(2,0));
-	for (int i = 0; i < n; i++) 
-		cin >> arr[i][0] >> arr[i][1];
 	
-	sort(arr.begin(), arr.end());
-	for (int i = 0; i < n; i++) {
-		cout << arr[i][0] << ' ' << arr[i][1] << '\n';
-	}
+	//좌표벡터 선언
+	//열벡터를 2로 초기화
+	vector<vector<int>> points(n,vector<int>(2,0));
+
+	//입력
+	for (int i = 0; i < n; i++) 
+		cin >> points[i][0] >> points[i][1];
+	
+	//정렬
+	sort(points.begin(), points.end());
+
+	//출력
+	for (int i = 0; i < n; i++) 
+		cout << points[i][0] << ' ' << points[i][1] << '\n';
+	
+	return;
 }
 
 int main() {
+	//초기화
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
 	solution();
 	return 0;
 }

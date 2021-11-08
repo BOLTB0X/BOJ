9c8a4b91177fe98280baa547cb2e11d4a69efa8b
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-11-08 17:05:41 +0900

    ABCDE
    https://www.acmicpc.net/problem/13023

--- Graph_Traversal/13023_ABCDE_DFS.cpp
+++ Graph_Traversal/13023_ABCDE_DFS.cpp
@@ -3,56 +3,68 @@
 
 using namespace std;
 
-int N, M;
-vector<int> graph[2001];
-bool visited[2001];
-//체크
-bool flag = false;
+bool flag;
 
-//입력
-void input(void) {
-	cin >> N >> M;
-	//노드 갯수만큼 반복
-	for (int i = 0; i < M; ++i) {
-		int a, b;
-		cin >> a >> b;
-		graph[a].push_back(b);
-		graph[b].push_back(a);
-	}
-	return;
-}
-
-//깊이 우선
-void DFS(int start, int depth) {
+//깊이우선탐색
+void DFS(vector<vector<int>>& adj, vector<bool>& visited, int start, int depth) {
 	visited[start] = true;
-	
-	//즉 친구관계 A,B,C,D,E 깊이가 4이므로
+
+	//깊이가 4까지 간다면
 	if (depth == 4) {
 		flag = true;
 		return;
 	}
-	for (int i = 0; i < graph[start].size(); ++i) {
-		int next = graph[start][i];
-		if (!visited[next]) {
-			DFS(next, depth + 1);
-			visited[next] = false;
-		}
+
+	//현재 노드에 연결되어 있는 노드 탐색
+	for (int i = 0; i < adj[start].size(); ++i) {
+		//다음 노드
+		int next = adj[start][i];
+		
+		//재방문인 경우
+		if (visited[next])
+			continue;
+
+		//다음 노드 깊이우선탐색
+		DFS(adj, visited, next, depth + 1);
+		visited[next] = false;
 	}
+
 	return;
 }
 
-//해결
 void solution(void) {
+	int n, m;
+
+	cin >> n >> m;
+
+	//인접그래프,방문리스트 생성 및 초기화
+	vector<vector<int>> adj_graph(n + 1);
+	vector<bool> visited(n + 1, false);
+
 	//입력
-	input();
-	//각 정점별 탐색
-	for (int i = 0; i < N; ++i) {
-		DFS(i, 0);
+	for (int i = 0; i < m; ++i) {
+		int a, b;
+		cin >> a >> b;
+		//무향이므로
+		adj_graph[a].push_back(b);
+		adj_graph[b].push_back(a);
+	}
+
+	for (int i = 0; i < n; ++i) {
+		DFS(adj_graph, visited, i, 0);
+		//호출 후 해당 노드 미방문 처리
 		visited[i] = false;
+		
+		//만약 answer가 true이면
 		if (flag)
 			break;
 	}
-	cout << flag << '\n';
+
+	if (flag)
+		cout << 1 << '\n';
+	else
+		cout << 0 << '\n';
+
 	return;
 }
 

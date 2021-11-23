5d04042507a31ebfd196797c7cb8be9bcf8c127d
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-11-23 16:37:17 +0900

    문자열 반복
    https://www.acmicpc.net/problem/2675

--- 파이썬배우기(001~050)/2675_문자열 반복.py
+++ 파이썬배우기(001~050)/2675_문자열 반복.py
@@ -1,8 +1,15 @@
-#문자열 반복 https://www.acmicpc.net/problem/2675
-t=int(input())
-for _ in range(t):
-    r,s=map(str,input().split())
-    p=''
-    for i in range(len(s)):
-        p=p+(int(r)*s[i])
-    print(p)
+import sys
+input = sys.stdin.readline
+
+T = int(input())
+while T != 0:
+    R,S = input().split()
+    #정답
+    result = ''
+    
+    for s in S:
+        result += (s *int(R))
+    
+    #출력
+    print(result)
+    T -= 1

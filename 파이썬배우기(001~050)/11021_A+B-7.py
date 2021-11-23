97dc5154e3073fb79616723f88286d4520c1d5ef
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-11-23 16:07:46 +0900

    A+B-7
    https://www.acmicpc.net/problem/11021

--- 파이썬배우기(001~050)/11021_A+B-7.py
+++ 파이썬배우기(001~050)/11021_A+B-7.py
@@ -1,8 +1,12 @@
-#A+B-7 https://www.acmicpc.net/problem/11021
-n=int(input())
+import sys
+input = sys.stdin.readline
+
+n = int(input())
 li=[]
+
 for i in range(n):
-    a,b=map(int,input().split())
+    a,b = map(int,input().split())
     li.append(a+b)
+    
 for i in range(n):
     print('Case #%i'%(i+1)+':',li[i])

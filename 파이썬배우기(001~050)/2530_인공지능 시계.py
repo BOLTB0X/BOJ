1f469fe0f70852306dff0a9d3a251abc3036413e
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-11-23 16:31:16 +0900

    인공지능 시계
    https://www.acmicpc.net/problem/2530

--- 파이썬배우기(001~050)/2530_인공지능 시계.py
+++ 파이썬배우기(001~050)/2530_인공지능 시계.py
@@ -1,15 +1,22 @@
-#인공지능 시계 https://www.acmicpc.net/problem/2530
+import sys
+input = sys.stdin.readline
+
 a,b,c=map(int,input().split())
-d=int(input()) #초단위로 입력받음
-c+=d%60 #d는 60을 넘으면 안되므로
-b+=d//60 #b에 60의 몫을 더해줌
-while a>=24 or b>=60 or c>=60: #조건에 맞게 
-    if c>=60:
-        b+=1
-        c-=60
-    if b>=60:
-        a+=1
-        b-=60
-    if a>=24:
-        a-=24
+#초단위로 입력받음
+d=int(input()) 
+#d는 60을 넘으면 안되므로
+c+=d%60 
+#b에 60의 몫을 더해줌
+b+=d//60 
+
+while a >= 24 or b >= 60 or c >= 60:
+    if c >= 60:
+        b += 1
+        c -= 60
+    if b >= 60:
+        a += 1
+        b -= 60
+    if a >= 24:
+        a -= 24
+        
 print(a,b,c)

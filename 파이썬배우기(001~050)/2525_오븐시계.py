66efe57a0e2408ea9ba630fbb7843485388fec78
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-11-23 16:17:39 +0900

    오븐 시계
    https://www.acmicpc.net/problem/2525

--- 파이썬배우기(001~050)/2525_오븐시계.py
+++ 파이썬배우기(001~050)/2525_오븐시계.py
@@ -1,21 +1,23 @@
-# 오븐시계 https://www.acmicpc.net/problem/2525
-a,b=map(int,input().split()) #현재 시간
-c=int(input()) #소요시간
-if (b+c)>=60: #만약 60분을 넘게되면
-    a+=(b+c)//60 #시간+
-    if a>23: #시간을 23이내로 표기해야므로
-        a-=24
-    b=(b+c)%60 #b도 60을 넘으면 안되므로
+import sys
+input = sys.stdin.readline
+
+#현재 시간
+a,b=map(int,input().split())
+#소요시간
+c=int(input())
+
+#분이 60을 넘어가게 되면
+if 60 <= (b+c):
+    #더해줌
+    a += (b+c)//60
+    #만약 23시를 넘게 되면
+    if a > 23:
+        a -= 24
+    b = (b+c)%60
+    
+    #시간 출력
     print(a,b)
+
+#그 외
 else:
     print(a,b+c)
-
-##다른 풀이
-a+=c//60
-b+=c%60
-if b>=60:
-    a+=1
-    b-=60
-if a>=24:
-    a-=24
-print(a,b)

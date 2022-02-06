edf7628cf244f2e2987b1119b9b2c45c95f42e6e
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-02-06 14:37:05 +0900

    골드바흐의 추측
    https://www.acmicpc.net/problem/6588

--- Math/6588_골드바흐의 추측.cpp
+++ Math/6588_골드바흐의 추측.cpp
@@ -1,17 +1,19 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <cstdio>
+#include <iostream>
 #include <cmath>
+
+using namespace std;
 
 const int MAX = 1000000;
 int prime[MAX];
 bool check[MAX + 1];
-int pn;
+int p_idx;
 
-int main() {
+int main(void ) {
+    //소수 만들기
     check[0] = check[1] = true;
     for (int i = 2; i * i <= MAX; i++) {
         if (check[i] == false) {
-            prime[pn++] = i;
+            prime[p_idx++] = i;
             for (int j = i + i; j <= MAX; j += i)
                 check[j] = true;
         }
@@ -19,10 +21,13 @@ int main() {
 
     while (true) {
         int n;
-        scanf("%d", &n);
+
+        cin >> n;
+
         if (n == 0)
             break;
-        for (int i = 0; i < pn; i++) {
+
+        for (int i = 0; i < p_idx; i++) {
             if (check[n - prime[i]] == false) {
                 printf("%d = %d + %d\n", n, prime[i], n - prime[i]);
                 break;

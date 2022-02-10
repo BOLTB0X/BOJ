080f6a11d652bb24a5b68a683c2148dc3fc1e08d
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-02-10 11:11:08 +0900

    1, 2, 3 더하기
    https://www.acmicpc.net/problem/9095

--- DP/9095_1, 2, 3 더하기.c
+++ DP/9095_1, 2, 3 더하기.c
@@ -2,6 +2,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int dp[12];
+
+void init(int n) {
+	for (int i = 0; i <= n; ++i) 
+		dp[i] = 0;
+
+	dp[1] = 1;
+	dp[2] = 2;
+	dp[3] = 4;
+
+	for (int i = 4; i <= n; ++i)
+		dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+	return;
+}
+
 int main(void) {
 	int T;
 	scanf("%d", &T);
@@ -9,19 +24,8 @@ int main(void) {
 	while (T--) {
 		int n;
 		scanf("%d", &n);
-		//동적할당 및 초기화
-		int* dp = (int*)malloc(sizeof(int) * (n + 1));
-		for (int i = 0; i <= n; ++i) {
-			dp[i] = 0;
-		}
-		dp[1] = 1;
-		dp[2] = 2;
-		dp[3] = 4;
-		for (int i = 4; i <= n; ++i) {
-			dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
-		}
+		init(n);	
 		printf("%d\n", dp[n]);
-		free(dp);
 	}
 	return 0;
 }

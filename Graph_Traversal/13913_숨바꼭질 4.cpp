d6432675741c90a439279eed315999a71d1f0294
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-02-13 15:07:31 +0900

    숨바꼭질 4
    https://www.acmicpc.net/problem/13913

---
+++ Graph_Traversal/13913_숨바꼭질 4.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#define MLN 100001
+
+using namespace std;
+
+vector<int> solution(int n, int k) {
+	vector<int> result; //결과
+	queue<int> que; //큐
+	vector<int> visited(MLN, 0); //방문리스트
+	vector<int> path(MLN, 0); //경로 저장
+
+	que.push(n);
+	visited[n] = 1;
+
+	while (!que.empty()) {
+		int cur = que.front();
+		que.pop();
+
+		if (cur == k) {
+			int idx = cur;
+
+			while (idx != n) {
+				result.push_back(idx);
+				idx = path[idx];
+			}
+			result.push_back(n);
+			break;
+		}
+
+		if (cur + 1 < MLN && visited[cur + 1] == 0) {
+			que.push(cur + 1);
+			visited[cur + 1] = 1;
+			path[cur + 1] = cur;
+		}
+		
+		if (cur - 1 >= 0 && visited[cur - 1] == 0) {
+			que.push(cur - 1);
+			visited[cur - 1] = 1;
+			path[cur - 1] = cur;
+		}
+		
+		if (cur * 2 < MLN && visited[cur * 2] == 0) {
+			que.push(cur * 2);
+			visited[cur * 2] = 1;
+			path[cur * 2] = cur;
+		}
+	}
+
+	return result;
+}
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	int n, k;
+	cin >> n >> k;
+
+	vector<int> ret = solution(n, k);
+	cout << ret.size() - 1 << '\n';
+	
+	for (int i = ret.size() - 1; i >= 0; --i)
+		cout << ret[i] << ' ';
+
+	return 0;
+}

fc06434e8a8ffb4f32947bc814acade725c503b4
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-02-22 12:52:10 +0900

    연속 합
    https://www.acmicpc.net/problem/1912

--- DP/1912_연속 합.cpp
+++ DP/1912_연속 합.cpp
@@ -1,34 +1,23 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-int arr[100000];
-int dp[100000] = { 0, };
-
-//최댓값 비교
-int max(int a, int b) {
+int MAX(int a, int b) {
 	return a > b ? a : b;
 }
 
-void solution(void) {
-	int n;
-	cin >> n;
+void solution(int n, vector<int>& arr) {
+	int answer = 0;
+	vector<int> dp(n, 0);
+	dp[0] = answer = arr[0];
 
-	for (int i = 0; i < n; ++i)
-		cin >> arr[i];
-
-	int max_value = arr[0];
-	dp[0] = arr[0];
-
-	//보텀업
 	for (int i = 1; i < n; ++i) {
-		dp[i] = max(arr[i], arr[i] + dp[i - 1]);
-
-		if (dp[i] > max_value)
-			max_value = dp[i];
+		dp[i] = MAX(arr[i] + dp[i - 1], arr[i]);
+		answer = MAX(answer, dp[i]);
 	}
 
-	cout << max_value << '\n';
+	cout << answer;
 	return;
 }
 
@@ -38,6 +27,15 @@ int main(void) {
 	cin.tie(0);
 	cout.tie(0);
 
-	solution();
+	int n;
+	vector<int> arr;
+
+	cin >> n;
+	arr.resize(n, 0);
+
+	for (int i = 0; i < n; ++i)
+		cin >> arr[i];
+
+	solution(n, arr);
 	return 0;
 }

6c9e68408b00e5a7ed9390e527c56333f8ef97f0
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-02-23 16:56:22 +0900

    배열 돌리기 1
    https://www.acmicpc.net/problem/16926

--- SImulation/16926_배열 돌리기 1.c
+++ SImulation/16926_배열 돌리기 1.c
@@ -4,6 +4,7 @@
 int board[301][301];
 int backup[301][301];
 
+//ì¶œë ¥
 void print_board(int n, int m) {
 	for (int i = 0; i < n; ++i) {
 		for (int j = 0; j < m; ++j)
@@ -14,42 +15,36 @@ void print_board(int n, int m) {
 
 void solution(int n, int m, int r) {
 	while (r--) {
+		// (y1,x1) ------ (y2, x2)
+		// (y4,x4) ------ (y3, x3)
 		int y1 = 0, x1 = 0;
-		int y2 = n - 1, x2 = 0;
+		int y2 = 0, x2 = m - 1;
 		int y3 = n - 1, x3 = m - 1;
-		int y4 = 0, x4 = m - 1;
+		int y4 = n - 1, x4 = 0;
 
-		while ((y1 < y2) && (x1 < x4)) {
-			int tmp = board[y2][x2];
-			//ÇÏ 
-			for (int i = y2; i > y1; --i)
-				board[i][x2] = board[i - 1][x2];
-			//ÁÂ
-			for (int j = x1; j < x4; ++j)
-				board[y1][j] = board[y1][j + 1];
-			//»ó
-			for (int i = y4; i < y3; ++i)
-				board[i][x4] = board[i + 1][x4];
-			//¿ì
-			for (int j = x3; j > x2; --j)
-				board[y3][j] = board[y3][j - 1];
+		while (y1 < y4 && x1 < x2) {
+			int tmp = board[y1][x1];
 
-			board[y2][x2 + 1] = tmp;
+			//ì—´ ì´ë™
+			for (int i = x1; i < x2; ++i)
+				board[y1][i] = board[y1][i + 1];
+			//í–‰ ì´ë™
+			for (int i = y2; i < y3; ++i)
+				board[i][x2] = board[i + 1][x2];
+			//ì•ˆ
+			for (int i = x3; i > x4; --i)
+				board[y3][i] = board[y3][i - 1];
+			for (int i = y4; i > y1; --i)
+				board[i][x4] = board[i - 1][x4];
+			board[y1 + 1][x4] = tmp;
 
-			y1++;
-			x1++;
-
-			y2--;
-			x2++;
-
-			y3--;
-			x3--;
-
-			y4++;
-			x4--;
+			y1++, x1++;
+			y2++, x2--;
+			y3--, x3--;
+			y4--, x4++;
 		}
-
 	}
+
 	return;
 }
 

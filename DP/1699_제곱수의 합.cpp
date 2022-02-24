d0e25b90255ba6475cedf4959f17d3ab542d1c7c
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-02-24 10:38:42 +0900

    제곱수의 합
    https://www.acmicpc.net/problem/1699

--- DP/1699_제곱수의 합.cpp
+++ DP/1699_제곱수의 합.cpp
@@ -3,6 +3,7 @@
 
 using namespace std;
 
+//최솟값반환
 int MIN(int a, int b) {
 	return a < b ? a : b;
 }
@@ -15,8 +16,9 @@ void solution(int n) {
 	for (int i = 0; i <= n; ++i)
 		dp[i] = i;
 	
-	//보텀업방식
+	//보텀업 방식
 	for (int i = 2; i <= n; ++i) {
+		//기존 dp테이블 값과 기존 i에 제곱값을 뺀 dp테이블 + 1 중 최솟값 반환
 		for (int j = 2; j * j <= i; ++j) {
 			dp[i] = MIN(dp[i], dp[i - j * j] + 1);
 		}

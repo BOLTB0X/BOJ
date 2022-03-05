f0cc96e622e7c4ff8590232c806423eebd8e3fc4
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-03-05 12:42:47 +0900

    수 이어 쓰기 1
    https://www.acmicpc.net/problem/1748

--- Brute Force/연습/1748_수 이어 쓰기 1.c
+++ Brute Force/연습/1748_수 이어 쓰기 1.c
@@ -5,10 +5,11 @@ int main(void) {
 	int n, idx = 1, result = 0;
 
 	scanf("%d", &n);
-
+	
+	//1, 10, 100이므로
 	while (idx <= n) {
-		result += (n - idx + 1);
-		idx *= 10;
+		result += (n - idx + 1);  //현재 idx랑 비교
+		idx *= 10; //10씩 곱해줌
 	}
 
 	printf("%d", result);

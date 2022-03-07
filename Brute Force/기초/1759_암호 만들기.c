f4eeb1611d83e5409a48c4156d138bc30a0bf03c
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-03-07 22:35:03 +0900

    암호 만들기
    https://www.acmicpc.net/problem/1759

--- Brute Force/기초/1759_암호 만들기.c
+++ Brute Force/기초/1759_암호 만들기.c
@@ -4,26 +4,29 @@
 #include <string.h>
 
 char input[15];
-char str[15];
 int visited[15] = { 0, };
 
 int compare(const void* a, const void* b) {
 	return strcmp((char*)a, (char*)b);
 }
 
+//단어가 되는 체크
 void check_Voca(int L, int C) {
 	int j_cnt = 0;
 	int m_cnt = 0;
 
 	for (int i = 0; i < C; ++i) {
 		if (visited[i] == 1) {
+			//모음 갯수 카운트
 			if (input[i] == 'a' || input[i] == 'e' || input[i] == 'i' || input[i] == 'o' || input[i] == 'u')
 				m_cnt++;
+			//자음 갯수 카운트
 			else
 				j_cnt++;
 		}
 	}
 
+	//조건이 맞다면
 	if (j_cnt >= 2 && m_cnt >= 1) {
 		for (int i = 0; i < C; ++i) {
 			if (visited[i] == 1)
@@ -34,7 +37,9 @@ void check_Voca(int L, int C) {
 	return;
 }
 
+//깊이우선 탐색
 void DFS(int L, int C, int cur, int level) {
+	//탈출 조건
 	if (level == L) {
 		check_Voca(L, C);
 		return;

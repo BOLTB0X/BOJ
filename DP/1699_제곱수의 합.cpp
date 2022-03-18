0e68c2635ba413426b9051ebe1e5a7e4cd060ab8
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-03-19 00:04:37 +0900

    Update 1699_제곱수의 합.cpp
    https://www.acmicpc.net/problem/1699

--- DP/1699_제곱수의 합.cpp
+++ DP/1699_제곱수의 합.cpp
@@ -10,18 +10,16 @@ int Min(int a, int b) {
 
 void solution(int n) {
 	int answer = 0;
-	vector<int> dp(n + 1, 0);
+	// 초기화
+	vector<int> dp(n + 1);
+	for (int i = 1; i <= n; ++i)
+		dp[i] = i; // 1, 1 1, 1 1 1, 1 1 1 1,......
 
-	// 1, 1 + 1, 1 + 1 + 1...식으로 초기값 셋팅
-	for (int i = 0; i <= n; ++i)
-		dp[i] = i;
-
-	//보텀업
-	for (int i = 2; i <= n; ++i) {
-		for (int j = 2; j * j <= i; ++j)
+	//보텀업 
+	for (int i = 1; i <= n; ++i) {
+		for (int j = 1; j * j <= i; ++j) 
 			dp[i] = Min(dp[i], dp[i - j * j] + 1);
 	}
-
 	cout << dp[n];
 	return;
 }

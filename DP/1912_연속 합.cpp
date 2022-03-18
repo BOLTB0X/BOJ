7b03e5ece09355496393f73a5c21fd3ea4cb9c48
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-03-19 00:03:46 +0900

    연속합
    https://www.acmicpc.net/problem/1912

--- DP/1912_연속 합.cpp
+++ DP/1912_연속 합.cpp
@@ -1,41 +1,42 @@
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
-int MAX(int a, int b) {
+int dp[100001];
+
+int Max(int a, int b) {
 	return a > b ? a : b;
 }
 
-void solution(int n, vector<int>& arr) {
+int solution(int n, int* arr) {
 	int answer = 0;
-	vector<int> dp(n, 0);
-	dp[0] = answer = arr[0];
 
-	for (int i = 1; i < n; ++i) {
-		dp[i] = MAX(arr[i] + dp[i - 1], arr[i]);
-		answer = MAX(answer, dp[i]);
+	// 초기화
+	for (int i = 0; i <= n; ++i)
+		dp[i] = 0;
+
+	dp[0] = answer = arr[1]; // 셋팅
+
+	// 보텀업
+	for (int i = 2; i <= n; ++i) {
+		dp[i] = Max(arr[i] + dp[i - 1], arr[i]);
+		answer = Max(answer, dp[i]);
 	}
 
-	cout << answer;
-	return;
+	return answer;
 }
 
 int main(void) {
-	//초기화
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	cout.tie(0);
-
 	int n;
-	vector<int> arr;
-
 	cin >> n;
-	arr.resize(n, 0);
-
-	for (int i = 0; i < n; ++i)
+	
+	int* arr = new int[n + 1];
+	for (int i = 1; i <= n; ++i)
 		cin >> arr[i];
 
-	solution(n, arr);
+	int ret = solution(n, arr);
+	cout << ret;
+
+	delete[] arr;
 	return 0;
 }

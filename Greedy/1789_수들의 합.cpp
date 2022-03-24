1cee9981e6ad13ebb9925b3c048517e92ab12135
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-03-24 11:17:58 +0900

    수들의 합
    https://www.acmicpc.net/problem/1789

--- Greedy/1789_수들의 합.cpp
+++ Greedy/1789_수들의 합.cpp
@@ -1,27 +1,33 @@
 #include <iostream>
-#define LL long long
 
 using namespace std;
 
-int main(void) {
-	LL S;
-	LL tot = 0;
-	cin >> S;
-	int max_number = 0, number = 1; // 결과와 더해지는 수
+long long solution(long long S) {
+	long long answer = 0;
+	long long tot = 0, max_number = 0, plus_number = 1;
 
 	while (1) {
-		tot += number;
+		tot += plus_number;
 		max_number++;
 
+		// 커진다면
 		if (tot > S) {
-			tot -= number;
 			max_number--;
 			break;
 		}
-		number++;
-	}
 
-	cout << max_number;
+		plus_number++; // 더해지는 수 증가
+	}
+	answer = max_number;
+	return answer;
+}
+
+int main(void) {
+	long long S;
+
+	cin >> S;
+	long long ret = solution(S);
+	cout << ret;
 
 	return 0;
 }

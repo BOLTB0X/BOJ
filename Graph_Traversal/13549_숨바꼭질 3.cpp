1f047013b095ff939d8d8b17ffdfe4cb58912c3f
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-03-25 15:03:12 +0900

    숨바꼭질 3
    https://www.acmicpc.net/problem/13549

--- Graph_Traversal/13549_숨바꼭질 3.cpp
+++ Graph_Traversal/13549_숨바꼭질 3.cpp
@@ -1,14 +1,13 @@
 #include <iostream>
 #include <vector>
 #include <queue>
-#define MLN 100001
+#define Max_Size 100001
 
 using namespace std;
 
-int solution(int n, int k) {
-	int result = 0;
+int BFS(int n, int k) {
 	deque<int> dq;
-	vector<int> visited(MLN, 0);
+	vector<int> visited(Max_Size, 0);
 
 	dq.push_back(n);
 	visited[n] = 1;
@@ -17,40 +16,42 @@ int solution(int n, int k) {
 		int cur = dq.front();
 		dq.pop_front();
 
-		if (cur == k) 
+		if (cur == k)
 			break;
-		
-		
-		if (cur * 2 < MLN && visited[cur * 2] == 0) {
+
+		if (cur * 2 < Max_Size && visited[cur * 2] == 0) {
 			visited[cur * 2] = visited[cur];
 			dq.push_front(cur * 2);
-		}
-
-		if (cur + 1 < MLN && visited[cur + 1] == 0) {
-			visited[cur + 1] = visited[cur] + 1;
-			dq.push_back(cur + 1);
 		}
 
 		if (cur - 1 >= 0 && visited[cur - 1] == 0) {
 			visited[cur - 1] = visited[cur] + 1;
 			dq.push_back(cur - 1);
 		}
-		
+			
+		if (cur + 1 < Max_Size && visited[cur + 1] == 0) {
+			visited[cur + 1] = visited[cur] + 1;
+			dq.push_back(cur + 1);
+		}
 	}
-	result = visited[k] - 1;
-	return result;
+
+	return (visited[k] - 1);
+}
+
+int solution(int n, int k) {
+	int answer = 0;
+
+	answer = BFS(n, k);
+
+	return answer;
 }
 
 int main(void) {
-	//초기화
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	cout.tie(0);
 	int n, k;
 
 	cin >> n >> k;
-
 	int ret = solution(n, k);
 	cout << ret;
+
 	return 0;
 }

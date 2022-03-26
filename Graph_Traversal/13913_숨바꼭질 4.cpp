c751b004c8e7a493164a0ee4fea8e821c2f00e96
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-03-26 12:50:15 +0900

    숨바꼭질 4
    https://www.acmicpc.net/problem/13913

--- Graph_Traversal/13913_숨바꼭질 4.cpp
+++ Graph_Traversal/13913_숨바꼭질 4.cpp
@@ -1,18 +1,19 @@
 #include <iostream>
-#include <vector>
 #include <queue>
-#define Max_Size 100001
+#include <vector>
+#define Max_Size 100001 // 최대길이
 
 using namespace std;
 
-vector<int> solution(int n, int k) {
-	vector<int> result; //결과
-	queue<int> que; //큐
-	vector<int> visited(Max_Size, 0); //방문리스트
-	vector<int> path(Max_Size, 0); //기록
+vector<int> BFS(int n, int k) {
+	vector<int> result;
+	vector<int> visited(Max_Size, 0);
+	vector<int> path(Max_Size, 0); // 자취
+	queue<int> que;
 
-	que.push(n);
 	visited[n] = 1;
+	que.push(n);
+	path.push_back(n);
 
 	while (!que.empty()) {
 		int cur = que.front();
@@ -25,25 +26,26 @@ vector<int> solution(int n, int k) {
 				result.push_back(idx);
 				idx = path[idx];
 			}
+
 			result.push_back(n);
 			break;
 		}
 
 		if (cur - 1 >= 0 && visited[cur - 1] == 0) {
 			visited[cur - 1] = 1;
-			path[cur - 1] = cur;
+			path[cur - 1] = cur; // 전 위치를 담음
 			que.push(cur - 1);
 		}
 		
 		if (cur + 1 < Max_Size && visited[cur + 1] == 0) {
 			visited[cur + 1] = 1;
-			path[cur + 1] = cur;
+			path[cur + 1] = cur; // 전 위치를 담음
 			que.push(cur + 1);
 		}
 		
-		if (cur * 2 < Max_Size && visited[cur * 2] == 0) {
+		if (cur *  2 < Max_Size && visited[cur * 2] == 0) {
 			visited[cur * 2] = 1;
-			path[cur * 2] = cur;
+			path[cur * 2] = cur; // 전 위치를 담음
 			que.push(cur * 2);
 		}
 	}
@@ -51,17 +53,19 @@ vector<int> solution(int n, int k) {
 	return result;
 }
 
+vector<int> solution(int n, int k) {
+	vector<int> answer = BFS(n, k);
+	return answer;
+}
+
 int main(void) {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	cout.tie(0);
-
 	int n, k;
-	cin >> n >> k;
 
+	cin >> n >> k;
 	vector<int> ret = solution(n, k);
+
+	// 출력
 	cout << ret.size() - 1 << '\n';
-	
 	for (int i = ret.size() - 1; i >= 0; --i)
 		cout << ret[i] << ' ';
 

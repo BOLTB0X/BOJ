d2787c35462aeb869b0ec61a563448827544a12e
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-03-28 17:47:18 +0900

    배열 돌리기 1
    https://www.acmicpc.net/problem/16926

--- SImulation/16926_배열 돌리기 1.cpp
+++ SImulation/16926_배열 돌리기 1.cpp
@@ -2,82 +2,74 @@
 
 using namespace std;
 
-int n, m, r;
-int arr[301][301] = { 0, };
+int arr[300][300];
 
-//회전방향
-void arr_rotate(void) {
+//시뮬레이션
+void arr_rotate(int n, int m, int r) {
 	while (r--) {
-		int y1 = 0;
-		int x1 = 0;
+		int y1 = 0, x1 = 0;
+		int y2 = n - 1, x2 = 0;
+		int y3 = n - 1, x3 = m - 1;
+		int y4 = 0, x4 = m - 1;
 
-		int y2 = n - 1;
-		int x2 = 0;
-
-		int y3 = n - 1;
-		int x3 = m - 1;
-
-		int y4 = 0;
-		int x4 = m - 1;
-
-		//시뮬시작
 		while ((y1 < y2) && (x1 < x4)) {
 			int tmp = arr[y2][x2];
-			//상
-			for (int i = y2; i > y1; i--) {
+			
+			// 상
+			for (int i = y2; i > y1; --i)
 				arr[i][x2] = arr[i - 1][x2];
-			}
-			//좌
-			for (int j = x1; j < x4; j++) {
-				arr[y1][j] = arr[y1][j + 1];
-			}
-			//하
-			for (int i = y4; i < y3; i++) {
+
+			// 좌
+			for (int i = x1; i < x4; ++i)
+				arr[y1][i] = arr[y1][i + 1];
+
+			// 하
+			for (int i = y4; i < y3; ++i)
 				arr[i][x4] = arr[i + 1][x4];
-			}
-			//우
-			for (int j = x3; j > x2; j--) {
-				arr[y3][j] = arr[y3][j-1];
-			}
+
+			// 우
+			for (int i = x3; i > x2; --i)
+				arr[y3][i] = arr[y3][i - 1];
+
 			arr[y2][x2 + 1] = tmp;
-			y1 += 1;
-			x1 += 1;
-			
-			y2 -= 1;
-			x2 += 1;
-			
-			y3 -= 1;
-			x3 -= 1;
-			
-			y4 += 1;
-			x4 -= 1;
+			y1++, x1++;
+			y2--, x2++;
+			y3--, x3--;
+			y4++, x4--;
 		}
 	}
+	
+	return;
 }
 
-void print_arr(void) {
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			cout << arr[i][j] << " ";
-		}
-		cout << "\n";
-	}
+//해결
+void solution(int n, int m, int r) {
+	//시뮬레이션 시작
+	arr_rotate(n, m, r);
+
 	return;
 }
 
 int main(void) {
-	//초기화
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	cout.tie(0);
+	int n, m, r;
 
+	//입력
 	cin >> n >> m >> r;
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			cin >> arr[i][j];
+
+	for (int y = 0; y < n; ++y) {
+		for (int x = 0; x < m; ++x) {
+			cin >> arr[y][x];
 		}
 	}
-	arr_rotate();
-	print_arr();
+
+	solution(n, m, r);
+
+	//출력
+	for (int y = 0; y < n; ++y) {
+		for (int x = 0; x < m; ++x) {
+			cout << arr[y][x] << ' ';
+		}
+		cout << '\n';
+	}
 	return 0;
 }

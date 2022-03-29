80305da93988d2453b974b7bac9308d9a6997e69
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-03-29 14:44:05 +0900

    최대공약수와 최소공배수
    https://www.acmicpc.net/problem/2609

--- Math/2609_최대공약수와 최소공배수.cpp
+++ Math/2609_최대공약수와 최소공배수.cpp
@@ -4,32 +4,27 @@ using namespace std;
 
 // 최대공약수
 int gcd(int a, int b) {
-	if (a % b == 0)
-		return b;
-	return gcd(b, a % b);
+	if (a < b)
+		swap(a, b);
+
+	while (b != 0) {
+		int r = a % b;
+		a = b;
+		b = r;
+	}
+
+	return a;
 }
 
 // 최소공배수
 int lcm(int a, int b) {
-	return (a * b) / gcd(a, b);
-}
-
-void solution(int a, int b) {
-	// 최대공약수, 최소공배수 
-	int ret1 = gcd(a, b);
-	int ret2 = lcm(a, b);
-
-	cout << ret1 << '\n';
-	cout << ret2 << '\n';
-
-	return;
+	return a * b / gcd(a, b);
 }
 
 int main(void) {
 	int a, b;
 
 	cin >> a >> b;
-	solution(a,b);
-	
+	cout << gcd(a, b) << '\n' << lcm(a, b);
 	return 0;
 }

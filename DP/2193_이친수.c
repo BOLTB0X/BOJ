a5f17a8c2de0e8811aacdd9169de254e5ca9e4bc
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-04-02 20:16:06 +0900

    이친수
    https://www.acmicpc.net/problem/2193

--- DP/2193_이친수.c
+++ DP/2193_이친수.c
@@ -1,22 +1,29 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <malloc.h>
+#include <stdlib.h>
+#define LL long long
+
+LL solution(int n) {
+	LL answer = 0;
+	LL* dp = malloc(sizeof(LL) * (n + 1));
+	
+	dp[0] = 0;
+	dp[1] = 1;
+	dp[2] = 1;
+	for (int i = 3; i <= n; ++i)
+		dp[i] = dp[i - 1] + dp[i - 2];
+
+	answer = dp[n];
+	free(dp);
+	return answer;
+}
 
 int main(void) {
 	int n;
 	scanf("%d", &n);
 
-	//동적할당
-	long long* dp = malloc(sizeof(long long) * (n + 1));
-	dp[1] = 1;
-	dp[2] = 1;
+	LL ret = solution(n);
+	printf("%lld", ret);
 
-	for (int i = 3; i <= n; ++i)
-		dp[i] = dp[i - 1] + dp[i - 2];
-
-	printf("%lld\n", dp[n]);
-
-	//메모리 해제
-	free(dp);
 	return 0;
 }

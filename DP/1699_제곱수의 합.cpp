c3ac7da3650faaa58dae06b86c2825b9408d8717
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-04-03 14:57:37 +0900

    제곱수의 합
    https://www.acmicpc.net/problem/1699

--- DP/1699_제곱수의 합.cpp
+++ DP/1699_제곱수의 합.cpp
@@ -3,36 +3,37 @@
 
 using namespace std;
 
-//최솟값반환
+// 최솟값반환
 int Min(int a, int b) {
 	return a < b ? a : b;
 }
 
-void solution(int n) {
+int solution(int n) {
 	int answer = 0;
-	// 초기화
+	
+	// dp 테이블 생성 및 초기화
 	vector<int> dp(n + 1);
 	for (int i = 1; i <= n; ++i)
-		dp[i] = i; // 1, 1 1, 1 1 1, 1 1 1 1,......
+		dp[i] = i; // 1, 1 1, 1 1 1, 1 1 1 1, ......
 
-	//보텀업 
+	// 보텀업
 	for (int i = 1; i <= n; ++i) {
-		for (int j = 1; j * j <= i; ++j) 
-			dp[i] = Min(dp[i], dp[i - j * j] + 1);
+		for (int j = 1; j *j<=n; ++j)
+			// 표현이 가능하다면
+			if (i - j * j >= 0) {
+				dp[i] = Min(dp[i], dp[i - j * j] + 1);
+			}
 	}
-	cout << dp[n];
-	return;
+
+	answer = dp[n];
+	return answer;
 }
 
 int main(void) {
-	//초기화
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	cout.tie(0);
-	
 	int n;
 	cin >> n;
 
-	solution(n);
+	int ret = solution(n);
+	cout << ret;
 	return 0;
 }

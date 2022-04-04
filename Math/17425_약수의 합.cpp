2c08ea519273bad5b16708fc7ad37f9dd52aa7ea
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-04-04 14:07:58 +0900

    약수의 합
    https://www.acmicpc.net/problem/17425

--- Math/17425_약수의 합.cpp
+++ Math/17425_약수의 합.cpp
@@ -8,31 +8,28 @@ using namespace std;
 vector<long long> dp;
 vector<long long> divisor_tot;
 
-void do_dp(void) {
+void do_dynamic(void) {
 	dp.resize(Max_Size, 0);
-	divisor_tot.resize(Max_Size, 1);
+	divisor_tot.resize(Max_Size, 1); // 1은 항상 있으므로
 
-	dp[1] = 1;
+	dp[1] = 1; // 1
 	for (LL i = 2; i < Max_Size; ++i) {
-		for (LL j = 1; j * i < Max_Size; ++j) {
+		for (LL j = 1; j * i < Max_Size; ++j)
 			divisor_tot[i * j] += i;
-		}
 		dp[i] = dp[i - 1] + divisor_tot[i];
 	}
-	
+
 	return;
 }
-
 
 int main(void) {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 	cout.tie(0);
-
 	int T, n;
 
 	cin >> T;
-	do_dp();
+	do_dynamic();
 	while (T--) {
 		cin >> n;
 		cout << dp[n] << '\n';

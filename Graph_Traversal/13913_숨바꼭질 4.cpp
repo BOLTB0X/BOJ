2b0367353f38194b113ec15b4f5a39da55a316a5
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-04-05 13:37:22 +0900

    숨바꼭질 4
    https://www.acmicpc.net/problem/13913

--- Graph_Traversal/13913_숨바꼭질 4.cpp
+++ Graph_Traversal/13913_숨바꼭질 4.cpp
@@ -6,14 +6,14 @@
 using namespace std;
 
 vector<int> BFS(int n, int k) {
+	vector<int> visited(Max_Size, 0); // 방문리스트
 	vector<int> result;
-	vector<int> visited(Max_Size, 0);
-	vector<int> path(Max_Size, 0); // 자취
+	vector<int> path(Max_Size, 0);
 	queue<int> que;
 
-	visited[n] = 1;
 	que.push(n);
 	path.push_back(n);
+	visited[n] = 1;
 
 	while (!que.empty()) {
 		int cur = que.front();
@@ -26,31 +26,31 @@ vector<int> BFS(int n, int k) {
 				result.push_back(idx);
 				idx = path[idx];
 			}
-
 			result.push_back(n);
-			break;
-		}
 
-		if (cur - 1 >= 0 && visited[cur - 1] == 0) {
-			visited[cur - 1] = 1;
-			path[cur - 1] = cur; // 전 위치를 담음
-			que.push(cur - 1);
+			return result;
 		}
 		
-		if (cur + 1 < Max_Size && visited[cur + 1] == 0) {
-			visited[cur + 1] = 1;
-			path[cur + 1] = cur; // 전 위치를 담음
-			que.push(cur + 1);
-		}
-		
-		if (cur *  2 < Max_Size && visited[cur * 2] == 0) {
-			visited[cur * 2] = 1;
-			path[cur * 2] = cur; // 전 위치를 담음
-			que.push(cur * 2);
+		else {
+			if (cur * 2 < Max_Size && visited[cur * 2] == 0) {
+				visited[cur * 2] = 1;
+				path[cur * 2] = cur;
+				que.push(cur * 2);
+			}
+
+			if (cur + 1 < Max_Size && visited[cur + 1] == 0) {
+				visited[cur + 1] = 1;
+				path[cur + 1] = cur;
+				que.push(cur + 1);
+			}
+			
+			if (cur - 1 >= 0 && visited[cur - 1] == 0) {
+				visited[cur -1] = cur;
+				path[cur - 1] = cur;
+				que.push(cur - 1);
+			}
 		}
 	}
-
-	return result;
 }
 
 vector<int> solution(int n, int k) {

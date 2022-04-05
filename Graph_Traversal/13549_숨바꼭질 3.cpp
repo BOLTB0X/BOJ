e462a27cac452ca8e08dd550cc7e6b44a75220a6
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-04-05 13:13:09 +0900

    숨바꼭질 3
    https://www.acmicpc.net/problem/13549

--- Graph_Traversal/13549_숨바꼭질 3.cpp
+++ Graph_Traversal/13549_숨바꼭질 3.cpp
@@ -16,22 +16,26 @@ int BFS(int n, int k) {
 		int cur = dq.front();
 		dq.pop_front();
 
+		// 도달한다면
 		if (cur == k)
 			break;
 
-		if (cur * 2 < Max_Size && visited[cur * 2] == 0) {
-			visited[cur * 2] = visited[cur];
-			dq.push_front(cur * 2);
-		}
+		else {
+			// 순간이동이면 앞에 삽입
+			if (cur * 2 < Max_Size && visited[cur * 2] == 0) {
+				visited[cur * 2] = visited[cur];
+				dq.push_front(cur * 2);
+			}
 
-		if (cur - 1 >= 0 && visited[cur - 1] == 0) {
-			visited[cur - 1] = visited[cur] + 1;
-			dq.push_back(cur - 1);
-		}
-			
-		if (cur + 1 < Max_Size && visited[cur + 1] == 0) {
-			visited[cur + 1] = visited[cur] + 1;
-			dq.push_back(cur + 1);
+			if (cur - 1 >= 0 && visited[cur - 1] == 0) {
+				visited[cur - 1] = visited[cur] + 1;
+				dq.push_back(cur - 1);
+			}
+
+			if (cur + 1 < Max_Size && visited[cur + 1] == 0) {
+				visited[cur + 1] = visited[cur] + 1;
+				dq.push_back(cur + 1);
+			}
 		}
 	}
 

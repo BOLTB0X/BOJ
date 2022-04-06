5cb56922132ced3dbe66c82c5603a33d0459420c
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-04-06 12:59:15 +0900

    킹
    https://www.acmicpc.net/problem/1063

--- SImulation/1063_킹.cpp
+++ SImulation/1063_킹.cpp
@@ -1,85 +1,87 @@
 #include <iostream>
 #include <string>
 #include <vector>
+
 using namespace std;
 
-string king, stone;
-int n;
-vector<string> move_plan;
-//R,L,B,T,RT,LT,RB,LB
-const int dr[8] = { 0,0,1,-1,-1,-1,1,1 };
-const int dc[8] = { 1,-1,0,0,1,-1,1,-1 };
+int board[8][8];
+// R, L, B, T, RT, LT, RB, LB
+const int dy[8] = { 0, 0, 1, -1, -1,-1,1,1 };
+const int dx[8] = { 1, -1, 0, 0,1,-1,1,-1 };
 
-bool is_range(int row, int col) {
-	if (row < 0 || col < 0 || row >= 8 || col >= 8) {
-		return false;
-	}
-	return true;
-}
-
-int turn_dir(string s) {
-	if (s == "R")
+int turn_dir(string c) {
+	if (c == "R")
 		return 0;
-	else if (s == "L")
+	else if (c == "L")
 		return 1;
-	else if (s == "B")
+	else if (c == "B")
 		return 2;
-	else if (s == "T")
+	else if (c == "T")
 		return 3;
-	else if (s == "RT")
+	else if (c == "RT")
 		return 4;
-	else if (s == "LT")
+	else if (c == "LT")
 		return 5;
-	else if (s == "RB")
+	else if (c == "RB")
 		return 6;
-	else if (s == "LB")
+	else if (c == "LB")
 		return 7;
 }
 
-void simulation(int kc, int sc, int kr, int sr) {
-	for (int i = 0; i < move_plan.size(); i++) {
-		int dir = turn_dir(move_plan[i]);
+vector<string> solution(string sk, string sst, int n, vector<string>& cmd) {
+	vector<string> answer(2);
+
+	int ky = '8' - sk[1];
+	int kx = sk[0] - 'A';
+
+	int sy = '8' - sst[1];
+	int sx = sst[0] - 'A';
+
+	// ì™•ì´ ë¨¼ì € ì›€ì§ì„
+	for (string& c : cmd) {
+		int dir = turn_dir(c);
+
+		int nky = ky + dy[dir];
+		int nkx = kx + dx[dir];
 		
-		int nkc = kc + dc[dir];
-		int nkr = kr + dr[dir];
-		//¹üÀ§ ¹ÛÀÌ¸é
-		if (!is_range(nkr, nkc))
+		if (nky < 0 || nkx < 0 || nky >= 8 || nkx >= 8)
 			continue;
-		//ÀÌµ¿ À§Ä¡°¡ µ¹ÀÇ À§Ä¡¸é
-		if (nkc == sc && nkr == sr) {
-			int nsc = sc + dc[dir];
-			int nsr = sr + dr[dir];
-			//¹üÀ§ ¹ÛÀÌ¸é
-			if (!is_range(nsr, nsc))
+		
+		if (nky == sy && nkx == sx) {
+			int nsy = sy + dy[dir];
+			int nsx = sx + dx[dir];
+
+			if (nsy < 0 || nsx < 0 || nsy >= 8 || nsx >= 8)
 				continue;
-			sc = nsc;
-			sr = nsr;
+
+			sy = nsy, sx = nsx;
 		}
-		kc = nkc;
-		kr = nkr;
+		ky = nky, kx = nkx;
 	}
-	cout << (char)(kc+'A') << 8-kr << '\n';
-	cout << (char)(sc+'A') << 8-sr << '\n';
-	return;
+
+	string ans1,ans2;
+	ans1.push_back((char)kx + 'A');
+	ans1.push_back(8 - ky + '0');
+	ans2.push_back((char)sx + 'A');
+	ans2.push_back((8 - sy + '0'));
+
+	answer[0] = ans1;
+	answer[1] = ans2;
+	return answer;
 }
 
 int main(void) {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	cout.tie(0);
+	string sy, sx;
+	int n;
+	vector<string> cmd;
 
-	int kc, sc, kr, sr;
-	cin >> king >> stone >> n;
+	cin >> sy >> sx >> n;
+	cmd.resize(n);
+	
+	for (int i = 0; i < n; ++i)
+		cin >> cmd[i];
 
-	kc = king[0] - 'A';
-	sc = stone[0] - 'A';
-	kr = ('8' - king[1]);
-	sr = ('8' - stone[1]);
-	for (int i = 0; i < n; i++) {
-		string cmd;
-		cin >> cmd;
-		move_plan.push_back(cmd);
-	}
-	simulation(kc, sc, kr, sr);
+	vector<string> ret = solution(sy, sx, n, cmd);
+	cout << ret[0] << '\n' << ret[1];
 	return 0;
 }

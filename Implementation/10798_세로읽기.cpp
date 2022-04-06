a996401a5e72d6ff2e38d417949e605956c7c0f3
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-04-06 10:10:54 +0900

    세로읽기
    https://www.acmicpc.net/problem/10798

---
+++ Implementation/10798_세로읽기.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+string solution(string str[]) {
+	string answer = "";
+	vector<vector<char>> board(5, vector<char>(15, 0)); // 2차원 문자형
+
+	// 보드로 넘겨줌
+	for (int i = 0; i < 5; ++i) {
+		for (int j = 0; j < str[i].length(); ++j) {
+			board[i][j] = str[i][j];
+		}
+	}
+	
+	for (int i = 0; i < 15; ++i) {
+		for (int j = 0; j < 5; ++j) {
+			if (board[j][i] == 0)
+				continue;
+
+			answer += str[j][i];
+		}
+	}
+
+	return answer;
+}
+
+int main(void) {
+	string str[15];
+	for (int i = 0; i < 5; ++i) 
+		cin >> str[i];
+	
+	string ret = solution(str);
+	cout << ret;
+	return 0;
+}

8baa7cb4d455b49ee371830e988a4187c0eaa6cb
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-04-07 16:41:06 +0900

    킹
    https://www.acmicpc.net/problem/1063

--- SImulation/1063_킹.cpp
+++ SImulation/1063_킹.cpp
@@ -5,25 +5,33 @@
 using namespace std;
 
 int board[8][8];
-// R, L, B, T, RT, LT, RB, LB
-const int dy[8] = { 0, 0, 1, -1, -1,-1,1,1 };
-const int dx[8] = { 1, -1, 0, 0,1,-1,1,-1 };
 
-int turn_dir(string c) {
+// R L B T RT LT RB LB
+const int dy[8] = { 0, 0, 1, -1, -1, -1, 1, 1};
+const int dx[8] = { 1, -1,0, 0, 1, -1, 1, -1 };
+
+int trans_cmd(string c) {
 	if (c == "R")
 		return 0;
+
 	else if (c == "L")
 		return 1;
+
 	else if (c == "B")
 		return 2;
+
 	else if (c == "T")
 		return 3;
+
 	else if (c == "RT")
 		return 4;
+
 	else if (c == "LT")
 		return 5;
+	
 	else if (c == "RB")
 		return 6;
+	
 	else if (c == "LB")
 		return 7;
 }
@@ -31,42 +39,39 @@ int turn_dir(string c) {
 vector<string> solution(string sk, string sst, int n, vector<string>& cmd) {
 	vector<string> answer(2);
 
-	int ky = '8' - sk[1];
-	int kx = sk[0] - 'A';
+	// 킹
+	int sx = sk[0] - 'A';
+	int sy = '8' - sk[1];
+	
+	// 돌
+	int tx = sst[0] - 'A';
+	int ty = '8' - sst[1];
 
-	int sy = '8' - sst[1];
-	int sx = sst[0] - 'A';
-
-	// 왕이 먼저 움직임
 	for (string& c : cmd) {
-		int dir = turn_dir(c);
+		int dir = trans_cmd(c);
+		int nsy = sy + dy[dir];
+		int nsx = sx + dx[dir];
 
-		int nky = ky + dy[dir];
-		int nkx = kx + dx[dir];
-		
-		if (nky < 0 || nkx < 0 || nky >= 8 || nkx >= 8)
+		if (nsy < 0 || nsx < 0 || nsy >= 8 || nsx >= 8)
 			continue;
-		
-		if (nky == sy && nkx == sx) {
-			int nsy = sy + dy[dir];
-			int nsx = sx + dx[dir];
 
-			if (nsy < 0 || nsx < 0 || nsy >= 8 || nsx >= 8)
+		if (nsy == ty && nsx == tx) {
+			int nty = ty + dy[dir];
+			int ntx = tx + dx[dir];
+
+			if (nty < 0 || ntx < 0 || nty >= 8 || ntx >= 8)
 				continue;
 
-			sy = nsy, sx = nsx;
+			ty = nty, tx = ntx;
 		}
-		ky = nky, kx = nkx;
+
+		sy = nsy, sx = nsx;
 	}
-
-	string ans1,ans2;
-	ans1.push_back((char)kx + 'A');
-	ans1.push_back(8 - ky + '0');
-	ans2.push_back((char)sx + 'A');
-	ans2.push_back((8 - sy + '0'));
-
-	answer[0] = ans1;
-	answer[1] = ans2;
+	
+	answer[0] += (sx + 'A');
+	answer[0] += (8 - sy + '0');
+	answer[1] += (tx + 'A');
+	answer[1] += (8 - ty + '0');
 	return answer;
 }
 

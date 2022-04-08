5606dc6daf821bdedd1d87549aeff4c599837bf8
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-04-08 10:18:25 +0900

    수 정렬하기 3
    https://www.acmicpc.net/problem/10989

--- Sorting/10989_수 정렬하기 3.cpp
+++ Sorting/10989_수 정렬하기 3.cpp
@@ -1,24 +1,33 @@
 #include <iostream> 
-using namespace std; 
 
-int main(void) { 
-	ios::sync_with_stdio(false); 
-	cin.tie(NULL); 
+using namespace std;
+
+int cnt[10001] = {0, };
+
+void solution(int n) {
+	for (int i = 1; i < 10001; ++i) {
+		if (cnt[i] > 0) {
+			for (int j = 0; j < cnt[i]; ++j)
+				cout << i << '\n';
+		}
+	}
+	return;
+}
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	int n, tmp;
+
+	cin >> n;
+	for (int i = 0; i < n; ++i) {
+		cin >> tmp;
+		cnt[tmp]++;
+	}
 	
-	int arr[10001] = { 0, }; 
-	int n; 
-	cin >> n; 
-	
-	for (int i = 0; i < n; i++) { 
-		int tmp; 
-		cin >> tmp; 
-		arr[tmp]++; 
-	} 
-	
-	for (int i = 1; i < 10001; i++) { 
-		if (arr[i]) 
-			for (int j = 0; j < arr[i]; j++) 
-				cout << i << '\n'; 
-	} 
+	solution(n);
+
 	return 0; 
 }

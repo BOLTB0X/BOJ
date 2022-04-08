5ba9dd80a71e21c760715d9ae31c4632884fc45c
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-04-08 13:28:59 +0900

    올림픽
    https://www.acmicpc.net/problem/8979

--- Implementation/8979_올림픽.cpp
+++ Implementation/8979_올림픽.cpp
@@ -1,49 +1,46 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
 
 using namespace std;
 
-struct Medal{
+int k_idx;
+
+struct Country {
 	int idx, gold, silver, bronze;
+
+	bool operator < (const Country& c) const {
+		if (gold == c.gold) {
+			if (silver == c.silver) {
+				if (bronze == c.bronze)
+					return idx == k_idx;
+				return bronze > c.bronze;
+			}
+			return silver > c.silver;
+		}
+		return gold > c.gold;
+	}
 };
 
-int idx;
-
-int compare(Medal& a, Medal& b) {
-	if (a.gold == b.gold) {
-		if (a.silver == b.silver) {
-			if (a.bronze == b.bronze)
-				// 동일 랭크에 대해 K번째 나라가 제일 먼저 오도록
-				return a.idx == idx;
-			return a.bronze > b.bronze;
-		}
-		return a.silver > b.silver;
-	}
-	return a.gold > b.gold;
-}
-
-int solution(int n, int k, Medal* arr) {
+int solution(int n, int k, Country* arr) {
 	int answer = 0;
-	idx = k;
-	sort(arr, arr + n, compare);
 
-	for (int i = 0; i <n;++i) {
+	k_idx = k;
+	sort(arr, arr + n);
+	for (int i = 0; i < n; ++i) {
 		if (arr[i].idx == k) {
 			answer = i + 1;
 			break;
 		}
 	}
-
 	return answer;
 }
 
 int main(void) {
 	int n, k;
-	Medal arr[1001];
+
+	Country arr[1001];
 
 	cin >> n >> k;
-
 	for (int i = 0; i < n; ++i) 
 		cin >> arr[i].idx >> arr[i].gold >> arr[i].silver >> arr[i].bronze;
 	

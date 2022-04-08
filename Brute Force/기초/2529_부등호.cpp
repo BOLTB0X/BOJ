791bbeac9812a0e16a92d991b7eb7ea496a61615
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-04-08 15:11:08 +0900

    부등호
    https://www.acmicpc.net/problem/2529

--- Brute Force/기초/2529_부등호.cpp
+++ Brute Force/기초/2529_부등호.cpp
@@ -1,19 +1,16 @@
 #include <iostream>
-#include <string> // to_string, atoi
+#include <string>
 #include <vector>
 #define LL long long
 
 using namespace std;
 
-char number[10] = { '0','1','2','3','4','5','6','7','8','9' };
-vector<int> visited(10, 0); // 방문리스트
+vector<int> visited;
 string max_result = "-9876543210";
 string min_result = "9876543210";
 
-// 깊이우선탐색
-void DFS(int n, vector<char>& v, string seq, int level) {
-	// 부등호에 들어가야하는 정수는 n + 1
-	if (seq.length() > n) {
+void DFS(int n, vector<char>& paren, string seq, int level) {
+	if (level == n + 1) {
 		LL tmp = stoll(seq);
 		if (stoll(max_result) < tmp)
 			max_result = seq;
@@ -22,28 +19,27 @@ void DFS(int n, vector<char>& v, string seq, int level) {
 		return;
 	}
 
-	// 0부터 9까지 순차적으로
 	for (int i = 0; i < 10; ++i) {
 		if (visited[i] == 0) {
 			// 없는 경우
 			if (level == 0) {
 				visited[i] = 1;
-				DFS(n, v, seq + number[i], level + 1);
+				DFS(n, paren, seq + to_string(i), level + 1);
 				visited[i] = 0;
 			}
-
 			else {
-				// 큰 경우
-				if (v[level - 1] == '>' && seq[level - 1] > number[i]) {
+				// 필터링
+				// level의 한 칸 전과 현재가 비교할 부등호
+				if (paren[level - 1] == '<' && seq[level - 1] - '0' < i) {
 					visited[i] = 1;
-					DFS(n, v, seq + number[i], level + 1);
+					DFS(n, paren, seq + to_string(i), level + 1);
 					visited[i] = 0;
 				}
 
-				// 작은 경우
-				else if (v[level - 1] == '<' && seq[level - 1] < number[i]) {
+	
+				if (paren[level - 1] == '>' && seq[level - 1]  - '0' > i) {
 					visited[i] = 1;
-					DFS(n, v, seq + number[i], level + 1);
+					DFS(n, paren, seq + to_string(i), level + 1);
 					visited[i] = 0;
 				}
 			}
@@ -52,26 +48,27 @@ void DFS(int n, vector<char>& v, string seq, int level) {
 	return;
 }
 
-pair<string, string> solution(int k, vector<char>& v) {
+pair<string, string> solution(int n, vector<char>& paren) {
 	pair<string, string> answer;
+	visited.resize(10, 0); // 방문리스트
 
-	//백트래킹
-	DFS(k, v, "", 0);
+	DFS(n, paren, "", 0);
+
 	answer.first = max_result;
 	answer.second = min_result;
 	return answer;
 }
 
 int main(void) {
-	int k;
-	vector<char> input;
+	int n;
+	vector<char> paren;
 
-	cin >> k;
-	input.resize(k);
-	for (int i = 0; i < k; ++i)
-		cin >> input[i];
+	cin >> n;
+	paren.resize(n);
+	for (int i = 0; i < n; ++i)
+		cin >> paren[i];
 
-	pair<string, string> ret = solution(k, input);
-	cout << ret.first << "\n" << ret.second << '\n';
+	pair<string, string> ret = solution(n, paren);
+	cout << ret.first << '\n' << ret.second;
 	return 0;
 }

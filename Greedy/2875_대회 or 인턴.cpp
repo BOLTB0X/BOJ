06297b3e8a48ba5276e8bbc5987400bdd3ed00bb
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-04-09 16:31:37 +0900

    대회 or 인턴
    https://www.acmicpc.net/problem/2875

---
+++ Greedy/2875_대회 or 인턴.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int solution(int n, int m, int k) {
+	int answer = 0;
+
+	while (1) {
+		// 여 2 이상 남 1 이상 참가 인원은 최소 3이므로
+		if (n < 2 || m < 1 || (n + m) - 3 < k)
+			break;
+		
+		n -= 2;
+		m--;
+		answer++;
+	}
+
+	return answer;
+}
+
+int main(void) {
+	int n, m, k;
+
+	cin >> n >> m >> k;
+
+	int ret = solution(n, m, k);
+	cout << ret;
+
+	return 0;
+}

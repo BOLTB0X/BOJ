dfd83142d9264d4368c675133ae6cf34859758c6
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-04-09 16:15:17 +0900

    수 정렬하기 5
    https://www.acmicpc.net/problem/15688

---
+++ Sorting/15688_수 정렬하기 5.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+
+using namespace std;
+
+void merge(int n, int* arr, int start, int end) {
+	int* sorted = new int[n];
+
+	int mid = (start + end) / 2;
+	int i = start, j = mid + 1, k = start;
+
+	while (i <= mid && j <= end) {
+		if (arr[i] <= arr[j])
+			sorted[k++] = arr[i++];
+		else
+			sorted[k++] = arr[j++];
+	}
+
+	if (i > mid) {
+		for (int a = j; a <= end; ++a)
+			sorted[k++] = arr[a];
+	}
+
+	else {
+		for (int a = i; a <= mid; ++a)
+			sorted[k++] = arr[a];
+	}
+
+	for (int a = start; a <= end; ++a)
+		arr[a] = sorted[a];
+
+	delete[] sorted;
+	return;
+}
+
+void merge_Sort(int n, int* arr, int start, int end) {
+	if (start < end) {
+		int mid = (start + end) / 2;
+		merge_Sort(n, arr, start, mid);
+		merge_Sort(n, arr, mid + 1, end);
+		merge(n, arr, start, end);
+	}
+	return;
+}
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	int n;
+	int* arr;
+
+	cin >> n;
+	arr = new int[n];
+	for (int i = 0; i < n; ++i)
+		cin >> arr[i];
+
+	merge_Sort(n, arr, 0, n - 1);
+
+	for (int i = 0; i < n; ++i)
+		cout << arr[i] << '\n';
+
+	delete[] arr;
+	return 0;
+}

ed58d26bb187b5ec57c708bee33c234624060adb
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-04-11 12:34:40 +0900

    단어 뒤집기
    https://www.acmicpc.net/problem/9093

--- DataStructure/9093_단어 뒤집기.cpp
+++ DataStructure/9093_단어 뒤집기.cpp
@@ -1,49 +1,53 @@
 #include <iostream>
-#include <stack>
 #include <string>
+#include <vector>
 
 using namespace std;
 
-void solution(void) {
-	int T;
-	stack<char> st;
+char stack[1001];
+int top;
 
-	cin >> T;
-
-	//입력 버퍼 비우기
-	cin.ignore();
-	while (T--) {
-		string words;
-		//띄어쓰기 까지 입력
-		getline(cin, words);
-		//출력을 맞추기 위해
-		words += " ";
-
-		//순회
-		for (int i = 0; i < words.length(); ++i) {
-			//공백이 스택의 pop 신호
-			if (words[i] == ' ') {
-				while (!st.empty()) {
-					cout << st.top();
-					st.pop();
-				}
-				cout << ' ';
-			}
-			//스택에 삽입
-			else 
-				st.push(words[i]);
-		}
-		cout << '\n';
-	}
+void push(char data) {
+	stack[++top] = data;
 	return;
 }
 
-int main(void) {
-	//초기화
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	cout.tie(0);
+vector<string> solution(string str) {
+	vector<string> answer;
+	string tmp = "";
+	top = -1; // 초기화
 
-	solution();
+	// 스택이용
+	for (int i = 0; i < str.length(); ++i) {
+		if (str[i] == ' ') {
+			while (top != -1) 
+				tmp += stack[top--];
+			answer.push_back(tmp);
+			tmp = "";
+		}
+		else
+			push(str[i]);
+	}
+	while (top != -1)
+		tmp += stack[top--];
+	answer.push_back(tmp);
+
+	return answer;
+}
+
+int main(void) {
+	int T;
+	
+	cin >> T;
+	cin.ignore();
+	while (T--) {
+		string str;
+		getline(cin, str);
+		vector<string> ret = solution(str);
+		for (int i = 0; i < ret.size(); ++i)
+			cout << ret[i] << ' ';
+		cout << '\n';
+	}
+
 	return 0;
 }

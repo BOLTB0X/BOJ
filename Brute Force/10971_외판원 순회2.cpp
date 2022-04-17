8ffec75daa8c6ca252c1efcb401173b95e4e4ce4
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-04-17 16:26:51 +0900

    외판원 순회 2
    https://www.acmicpc.net/problem/10971

--- Brute Force/10971_외판원 순회2.cpp
+++ Brute Force/10971_외판원 순회2.cpp
@@ -3,58 +3,62 @@
 
 using namespace std;
 
-int result = 0x7fffffff; // 최소를 위한
+int result = 1e9;
+int board[11][11];
+vector<int> visited;
 
 // 최솟값 비교
 int Min(int a, int b) {
 	return a < b ? a : b;
 }
 
-// 순열
-void DFS(int n, vector<vector<int>>& board, vector<int>& visited, int start, int cur, int tot, int level) {
-	if (level == n - 1) {
-		if (board[cur][start] != 0) 
-			result = Min(result, tot + board[cur][start]);
+// 다 방문했는지 체크
+bool is_All_visit(int n) {
+	for (int i = 1; i < n; ++i)
+		if (visited[i] == 0) return 0;
+
+	return 1;
+}
+
+void DFS(int n, int cur, int tot) {
+	if (is_All_visit(n) && board[cur][0] != 0) {
+		result = Min(result, tot + board[cur][0]);
 		return;
 	}
 
-	for (int i = 0; i < n; ++i) {
-		if (board[cur][i] != 0 && visited[i] == 0) {
-			visited[i] = 1;
-			DFS(n, board, visited, start, i ,tot + board[cur][i], level + 1);
-			visited[i] = 0;
-		}
+	for (int i = 1; i < n; ++i) {
+		if (visited[i] == 1)
+			continue;
+
+		if (board[cur][i] == 0)
+			continue;
+
+		visited[i] = 1;
+		DFS(n, i, tot + board[cur][i]);
+		visited[i] = 0;
 	}
 	return;
 }
 
-int solution(int n, vector<vector<int>>& board) {
+int solution(int n) {
 	int answer = 0;
-	vector<int> visited(n + 1, 0); // 방문리스트
+	visited = vector<int>(n, 0); // 초기화
 
-	// 각 행을 순열로 뽑아 각 비용을 비교
-	for (int i = 0; i < n; ++i) {
-		visited[i] = 1;
-		DFS(n, board, visited, i, i, 0, 0);
-		visited[i] = 0;
-	}
-
+	DFS(n, 0, 0);
 	answer = result;
 	return answer;
 }
 
 int main(void) {
 	int n;
-	vector<vector<int>> board;
 
 	cin >> n;
-	board.resize(n, vector<int>(n, 0));
 	for (int i = 0; i < n; ++i) {
-		for (int j = 0; j < n; ++j)
+		for (int j =0; j < n; ++j)
 			cin >> board[i][j];
 	}
 
-	int ret = solution(n, board);
+	int ret = solution(n);
 	cout << ret;
 	return 0;
 }

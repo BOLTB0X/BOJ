8ef1e80b947735aa4174a9a08e7daf42588e29df
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-04-17 16:04:34 +0900

    모든 순열
    https://www.acmicpc.net/problem/10974

--- Brute Force/10974_모든순열_BackTraking.cpp
+++ Brute Force/10974_모든순열_BackTraking.cpp
@@ -3,45 +3,46 @@
 
 using namespace std;
 
-int n;
-vector<int> per;
-bool visited[9] = { false, };
+vector<vector<int>> result;
+vector<bool> visited;
 
-void print_vector() {
-	for (int i = 0; i < n; ++i) {
-		cout << per[i] << ' ';
-	}
-	cout << '\n';
-	return;
-}
-
-void DFS(int depth) {
-	if (depth == n) {
-		print_vector();
+void DFS(int n, vector<int>& per, int level) {
+	if (level == n) {
+		result.push_back(per);
 		return;
 	}
+
 	for (int i = 1; i <= n; ++i) {
-		if (visited[i])
+		if (visited[i] == 1)
 			continue;
-		visited[i] = true;
+		visited[i] = 1;
 		per.push_back(i);
-		DFS(depth + 1);
-		visited[i] = false;
+		DFS(n, per, level + 1);
 		per.pop_back();
+		visited[i] = 0;
 	}
+	return;
 }
 
-void permutation(void) {
-	cin >> n;
-	DFS(0);
-	return;
+vector<vector<int>> solution(int n) {
+	vector<vector<int>> answer;
+	visited.resize(n + 1, 0);
+
+	vector<int> per;
+	DFS(n, per, 0);
+	answer = result;
+	return answer;
 }
 
 int main(void) {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	cout.tie(0);
+	int n;
 
-	permutation();
+	cin >> n;
+	vector<vector<int>> ret = solution(n);
+	for (auto& r : ret) {
+		for (int i = 0; i < n; ++i)
+			cout << r[i] << ' ';
+		cout << '\n';
+	}
 	return 0;
 }

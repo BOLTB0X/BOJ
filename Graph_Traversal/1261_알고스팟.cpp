e7913db8697265abe09e1eb5a97165469b2cee5e
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2022-04-19 16:23:46 +0900

    알고스팟
    https://www.acmicpc.net/problem/1261

--- Graph_Traversal/1261_알고스팟.cpp
+++ Graph_Traversal/1261_알고스팟.cpp
@@ -1,19 +1,23 @@
-#include <iostream>
-#include <vector>
-#include <queue>
+#include<iostream>
+#include<vector>
+#include<queue>
 #define INF 1e9
 
 using namespace std;
 
+int N, M;
+int board[100][100];
+
+// 상 우 하 좌
 const int dy[4] = { 1,0,-1,0 };
 const int dx[4] = { 0,1,0,-1 };
 
-int solution(int n, int m, vector<vector<int>>& board) {
+int solution(void) {
 	int answer = 0;
-	vector<vector<int>> dist(n, vector<int>(m, INF));
+	vector<vector<int>> dist(N, vector<int>(M, INF));
 	queue<pair<int, int>> que;
 
-	que.push({0,0});
+	que.push({ 0,0 });
 	dist[0][0] = 0;
 
 	while (!que.empty()) {
@@ -25,45 +29,42 @@ int solution(int n, int m, vector<vector<int>>& board) {
 			int ny = cy + dy[dir];
 			int nx = cx + dx[dir];
 
-			if (ny < 0 || nx < 0 || ny >= n || nx >= m)
+			if (ny < 0 || nx < 0 || ny >= N || nx >= M)
 				continue;
 
 			// 벽인 경우
 			if (board[ny][nx] == 1) {
 				if (dist[ny][nx] > dist[cy][cx] + 1) {
 					dist[ny][nx] = dist[cy][cx] + 1;
-					que.push({ ny,nx });
+					que.push({ ny, nx });
 				}
 			}
 
+			// 뚫인 경우
 			else if (board[ny][nx] == 0) {
 				if (dist[ny][nx] > dist[cy][cx]) {
 					dist[ny][nx] = dist[cy][cx];
-					que.push({ ny,nx });
+					que.push({ ny, nx });
 				}
 			}
 		}
 	}
 
-	answer = dist[n - 1][m - 1];
+	answer = dist[N - 1][M - 1];
 	return answer;
 }
 
 int main(void) {
-	int m, n;
 	string tmp;
-	vector<vector<int>> board;
 
-	cin >> m >> n;
-	board.resize(n, vector<int>(m, 0));
-
-	for (int i = 0; i < n; ++i) {
+	cin >> M >> N;
+	for (int i = 0; i < N; ++i) {
 		cin >> tmp;
-		for (int j = 0; j < m; ++j)
+		for (int j = 0; j < M; ++j)
 			board[i][j] = tmp[j] - '0';
 	}
 
-	int ret = solution(n, m, board);
+	int ret = solution();
 	cout << ret;
 	return 0;
 }
